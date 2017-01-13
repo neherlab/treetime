513a0f2e910cc24692be37a51ceebc02829c37d1
Richard Neher <richard.neher@tuebingen.mpg.de>
2017-01-13 16:15:21 +0100

    only optimize skyline in last iteration -- speeds things up and allows polytomy resolution to complete before coalescence is estimated

--- treetime/treetime.py
+++ treetime/treetime.py
@@ -54,12 +54,13 @@ class TreeTime(ClockTree):
                 self.logger('TreeTime.run: adding coalescent prior with Tc='+str(Tc),1)
                 self.merger_model = Coalescent(self.tree, Tc=avg_root_to_tip,
                                                date2dist=self.date2dist, logger=self.logger)
-                if Tc=='opt':
-                    self.merger_model.optimize_Tc()
-                    self.logger("optimized Tc to %f"%self.merger_model.Tc.y[0], 2)
-                elif Tc=='skyline':
-                    self.merger_model.optimize_skyline(**kwargs)
-                    self.logger("optimized a skyline ", 2)
+                if niter==max_iter-1:
+                    if Tc=='opt':
+                        self.merger_model.optimize_Tc()
+                        self.logger("optimized Tc to %f"%self.merger_model.Tc.y[0], 2)
+                    elif Tc=='skyline':
+                        self.merger_model.optimize_skyline(**kwargs)
+                        self.logger("optimized a skyline ", 2)
                 else:
                     try:
                         self.set_Tc(Tc)
