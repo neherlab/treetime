fix normalization of marginal profiles, avoid underflow error

treetime/treeanc.py
@@ -504,11 +504,15 @@ class TreeAnc(object):
             for c in node.up.clades:
                 if c != node:
                     tmp_msg*=c.marginal_Lx
+            norm_vector = tmp_msg.sum(axis=1)
+            tmp_msg=(tmp_msg.T/norm_vector).T
             node.seq_msg_from_parent = self.gtr.propagate_profile(tmp_msg,
                                             self._branch_length_to_gtr(node), return_log=False)
             node.marginal_profile = node.marginal_subtree_LH * node.seq_msg_from_parent
 
-            # choose sequence based maximal marginal LH. THIS NORMALIZES marginal_profile in place
+            norm_vector = node.marginal_profile.sum(axis=1)
+            node.marginal_profile=(node.marginal_profile.T/norm_vector).T
+            # choose sequence based maximal marginal LH.
             seq, prof_vals, idxs = seq_utils.prof2seq(node.marginal_profile, self.gtr,
                                                       sample_from_prof=other_sample_from_profile)
             node.mutations = [(anc, pos, der) for pos, (anc, der) in
@@ -527,7 +531,7 @@ class TreeAnc(object):
             self.store_compressed_sequence_pairs()
 
         # do clean-up:
-        if not debug:
+        if not self.debug:
             for node in self.tree.find_clades():
                 del node.marginal_subtree_LH
                 del node.marginal_subtree_LH_prefactor
@@ -640,7 +644,7 @@ class TreeAnc(object):
             self.store_compressed_sequence_pairs()
 
         # do clean-up
-        if not debug:
+        if not self.debug:
             for node in self.tree.find_clades(order='preorder'):
                 del node.joint_Lx
                 del node.joint_Cx