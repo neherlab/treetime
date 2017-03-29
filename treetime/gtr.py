5abc9d1ec9a9bec4da5dfedadccfa836dc330528
Pavel Sagulenko <pavel.sagulenko@tuebingen.mpg.de>
2017-03-29 13:06:28 +0200

    Empty commit, formatting only

--- treetime/gtr.py
+++ treetime/gtr.py
@@ -7,16 +7,16 @@ from seq_utils import alphabets, profile_maps
 
 def JC69 (mu=1.0, alphabet="nuc", **kwargs):
     """
-    Jukes-Cantor 1969 model. This model assumes equal concentrations 
-    of the nucleotides and equal transition rates between nucleotide states. 
+    Jukes-Cantor 1969 model. This model assumes equal concentrations
+    of the nucleotides and equal transition rates between nucleotide states.
     For more info, see: Jukes and Cantor (1969). Evolution of Protein Molecules. New York: Academic Press. pp. 21–132
-    
-    Args: 
-     
-     - mu(float): mutation rate 
-     
-     - alphabet(str): specify alphabet to use. 
-     Available alphabets are:  
+
+    Args:
+
+     - mu(float): mutation rate
+
+     - alphabet(str): specify alphabet to use.
+     Available alphabets are:
         'nuc' - nucleotides only, gaps ignored
         'nuc_gap' - nucleotide alphabet with gaps, gaps can be ignored optionally
     """
@@ -28,21 +28,21 @@ def JC69 (mu=1.0, alphabet="nuc", **kwargs):
 
 def K80(mu=1., kappa=0.1, **kwargs):
     """
-    Kimura 1980 model. Assumes equal concentrations across nucleotides, but 
-    allows different rates between transitions and transversions. The ratio  
-    of the transversion/transition rates is given by kappa parameter. 
-    For more info, see 
-    Kimura (1980),  J. Mol. Evol. 16 (2): 111–120. doi:10.1007/BF01731581. 
-    
-    Current implementation of the model does not account for the gaps. 
+    Kimura 1980 model. Assumes equal concentrations across nucleotides, but
+    allows different rates between transitions and transversions. The ratio
+    of the transversion/transition rates is given by kappa parameter.
+    For more info, see
+    Kimura (1980),  J. Mol. Evol. 16 (2): 111–120. doi:10.1007/BF01731581.
 
-    Args: 
+    Current implementation of the model does not account for the gaps.
 
-     - mu(float): overall mutation rate 
+    Args:
+
+     - mu(float): overall mutation rate
 
      - kappa(float): ratio of transversion/transition rates
     """
-    
+
     num_chars = len(alphabets['nuc'])
     W = _create_transversion_transition_W(kappa)
     gtr = GTR(alphabet=alphabets['nuc'])
@@ -51,27 +51,27 @@ def K80(mu=1., kappa=0.1, **kwargs):
 
 def F81(mu=1.0, pi=np.array([0.25, 0.25, 0.25, 0.25]), alphabet="nuc"):
     """
-    Felsenstein 1981 model. Assumes non-equal concentrations across nucleotides, 
-    but the transition rate between all states is assumed to be equal. See 
+    Felsenstein 1981 model. Assumes non-equal concentrations across nucleotides,
+    but the transition rate between all states is assumed to be equal. See
     Felsenstein (1981), J. Mol. Evol. 17  (6): 368–376. doi:10.1007/BF01734359
     for details.
 
-    Current implementation of the model does not account for the gaps (treatment of 
+    Current implementation of the model does not account for the gaps (treatment of
     gaps as characters is possible if specify alphabet='nuc_gap').
 
     Args:
 
-     - mu(float): mutation rate 
+     - mu(float): mutation rate
 
      - pi(numpy array): nucleotide concentrations
 
-     - alphabet(str): alphabet to use. Default 'nuc', which discounts al gaps. 
+     - alphabet(str): alphabet to use. Default 'nuc', which discounts al gaps.
      'nuc-gap' alphabet enables treatmen of gaps as characters.
     """
-    
+
     num_chars = len(alphabets[alphabet])
-    
-    if num_chars != pi.shape[0] : 
+
+    if num_chars != pi.shape[0] :
         raise ValueError("The number of the characters in the alphabet does not match the "
             "shape of the concentration vector.")
 
@@ -83,25 +83,25 @@ def F81(mu=1.0, pi=np.array([0.25, 0.25, 0.25, 0.25]), alphabet="nuc"):
 
 def HKY85(mu=1.0, pi=np.array([0.25, 0.25, 0.25, 0.25]), kappa=0.1):
     """
-    Hasegawa, Kishino and Yano 1985 model. Allows different concentrations of the 
-    nucleotides (as in F81) + distinguishes between transition/transversionmutations 
-    (similar to K80). Link: 
+    Hasegawa, Kishino and Yano 1985 model. Allows different concentrations of the
+    nucleotides (as in F81) + distinguishes between transition/transversionmutations
+    (similar to K80). Link:
     Hasegawa, Kishino, Yano (1985), J. Mol. Evol. 22 (2): 160–174. doi:10.1007/BF02101694
 
     Current implementation of the model does not account for the gaps
 
-    Args: 
+    Args:
 
-     - mu(float): mutation rate 
+     - mu(float): mutation rate
 
      - pi(numpy array): nucleotide concentrations
 
      - kappa(float): ratio of transversion/transition mutation rates
 
     """
-    
+
     num_chars = len(alphabets['nuc'])
-    if num_chars != pi.shape[0] : 
+    if num_chars != pi.shape[0] :
         raise ValueError("The number of the characters in the alphabet does not match the "
                          "shape of the concentration vector.")
 
@@ -113,22 +113,22 @@ def HKY85(mu=1.0, pi=np.array([0.25, 0.25, 0.25, 0.25]), kappa=0.1):
 
 def T92(mu=1.0, pi_GC=0.5, kappa=0.1):
     """
-    Tamura 1992 model. Extending Kimura  (1980) model for the case where a 
+    Tamura 1992 model. Extending Kimura  (1980) model for the case where a
     G+C-content bias exists. Link:
     Tamura K (1992),  Mol.  Biol. Evol. 9 (4): 678–687.  DOI: 10.1093/oxfordjournals.molbev.a040752
-    
+
     Current implementation of the model does not account for the gaps
-    
-    Args: 
-     
-     - mu(float): mutation rate 
 
-     - pi_GC(float): relative GC content 
+    Args:
 
-     - kappa(float): relative transversion/transition rate 
+     - mu(float): mutation rate
+
+     - pi_GC(float): relative GC content
+
+     - kappa(float): relative transversion/transition rate
 
     """
-    
+
     W = _create_transversion_transition_W(kappa)
     # A C G T
     if pi_CG >=1.:
@@ -140,9 +140,9 @@ def T92(mu=1.0, pi_GC=0.5, kappa=0.1):
 
 def TN93(mu=1.0, kappa1=1., kappa2=1., pi=np.array([0.25, 0.25, 0.25, 0.25])):
     """
-    Tamura and Nei 1993. The model distinguishes between the two different types of 
-    transition: (A <-> G) is allowed to have a different rate to (C<->T). 
-    Transversions have the same rate. The frequencies of the nucleotides are allowed 
+    Tamura and Nei 1993. The model distinguishes between the two different types of
+    transition: (A <-> G) is allowed to have a different rate to (C<->T).
+    Transversions have the same rate. The frequencies of the nucleotides are allowed
     to be different. Link:
     Tamura, Nei (1993), MolBiol Evol. 10 (3): 512–526. DOI:10.1093/oxfordjournals.molbev.a040023
 
@@ -150,15 +150,15 @@ def TN93(mu=1.0, kappa1=1., kappa2=1., pi=np.array([0.25, 0.25, 0.25, 0.25])):
 
      - mu(float): mutaion rate
 
-     - kappa1(float): relative A<-->C, A<-->T, T<-->G and G<-->C rates 
+     - kappa1(float): relative A<-->C, A<-->T, T<-->G and G<-->C rates
 
-     - kappa2(float): relative C<-->T rate 
+     - kappa2(float): relative C<-->T rate
 
     Note:
 
-     - Rate of A<-->G mutation is set to one. All other rates (kappa1, kappa2) 
+     - Rate of A<-->G mutation is set to one. All other rates (kappa1, kappa2)
     are specified relative to this rate
-    
+
     """
 
     W = np.ones((4,4))
@@ -167,17 +167,17 @@ def TN93(mu=1.0, kappa1=1., kappa2=1., pi=np.array([0.25, 0.25, 0.25, 0.25])):
         [kappa1, 1,      kappa1, kappa2],
         [1,      kappa1, 1,      kappa1],
         [kappa1, kappa2, kappa1,  1]])
-    
+
     pi /=pi.sum()
     num_chars = len(alphabets['nuc'])
-    if num_chars != pi.shape[0] : 
+    if num_chars != pi.shape[0] :
         raise ValueError("The number of the characters in the alphabet does not match the "
                          "shape of the concentration vector.")
-    
+
     gtr = GTR(alphabet=alphabets['nuc'])
     gtr.assign_rates(mu=mu, pi=pi, W=W)
     return gtr
-    
+
 def _create_transversion_transition_W(kappa):
     """
     Alphabet = [A, C, G, T]
@@ -328,129 +328,129 @@ class GTR(object):
         gtr.assign_rates(mu=mu, pi=pi, W=W)
         return gtr
 
-    @staticmethod 
+    @staticmethod
     def create_standard_model(model, **kwargs):
         """
-        Create standard model of molecular evolution. 
+        Create standard model of molecular evolution.
 
-        Args: 
-         - model(str): model to create. List of the available models is 
-         (see description below): ['JC69',   
+        Args:
+         - model(str): model to create. List of the available models is
+         (see description below): ['JC69',
 
-        Kwargs: 
+        Kwargs:
          - model arguments
 
         Available models:
-         
-         --- JC69: 
-            Jukes-Cantor 1969 model. This model assumes equal concentrations 
-            of the nucleotides and equal transition rates between nucleotide states. 
+
+         --- JC69:
+            Jukes-Cantor 1969 model. This model assumes equal concentrations
+            of the nucleotides and equal transition rates between nucleotide states.
             For more info, see: Jukes and Cantor (1969). Evolution of Protein Molecules. New York: Academic Press. pp. 21–132.
 
             To create this model, use:
-            
-            Args: model='JC69' - keyword to specify the model
-            
-            Kwargs: 
 
-             - mu(float)  - specify the mutation rate  
+            Args: model='JC69' - keyword to specify the model
+
+            Kwargs:
+
+             - mu(float)  - specify the mutation rate
 
              - alphabet(str) - alphabet. By default 'nuc' is used (all gaps are ignored).
              specify 'nuc_gap' to treat gaps as characters
 
-         
+
          --- K80:
-            Kimura 1980 model. Assumes equal concentrations across nucleotides, but 
-            allows different rates between transitions and transversions. The ratio  
-            of the transversion/transition rates is given by kappa parameter. 
-            For more info, see 
-            Kimura (1980),  J. Mol. Evol. 16 (2): 111–120. doi:10.1007/BF01731581. 
-            
-            Current implementation of the model does not account for the gaps. 
-        
-            Kwargs: 
-        
-             - mu(float): overall mutation rate 
-        
+            Kimura 1980 model. Assumes equal concentrations across nucleotides, but
+            allows different rates between transitions and transversions. The ratio
+            of the transversion/transition rates is given by kappa parameter.
+            For more info, see
+            Kimura (1980),  J. Mol. Evol. 16 (2): 111–120. doi:10.1007/BF01731581.
+
+            Current implementation of the model does not account for the gaps.
+
+            Kwargs:
+
+             - mu(float): overall mutation rate
+
              - kappa(float): ratio of transversion/transition rates
 
-         
+
          --- F81:
-            Felsenstein 1981 model. Assumes non-equal concentrations across nucleotides, 
-            but the transition rate between all states is assumed to be equal. See 
+            Felsenstein 1981 model. Assumes non-equal concentrations across nucleotides,
+            but the transition rate between all states is assumed to be equal. See
             Felsenstein (1981), J. Mol. Evol. 17  (6): 368–376. doi:10.1007/BF01734359
             for details.
-        
-            Current implementation of the model does not account for the gaps (treatment of 
+
+            Current implementation of the model does not account for the gaps (treatment of
             gaps as characters is possible if specify alphabet='nuc_gap').
-        
+
             Args:
-        
-             - mu(float): mutation rate 
-        
+
+             - mu(float): mutation rate
+
              - pi(numpy array): nucleotide concentrations
-        
-             - alphabet(str): alphabet to use. Default 'nuc', which discounts al gaps. 
+
+             - alphabet(str): alphabet to use. Default 'nuc', which discounts al gaps.
              'nuc-gap' alphabet enables treatmen of gaps as characters.
 
 
-        
+
          --- HKY85:
-            Hasegawa, Kishino and Yano 1985 model. Allows different concentrations of the 
-            nucleotides (as in F81) + distinguishes between transition/transversionmutations 
-            (similar to K80). Link: 
+            Hasegawa, Kishino and Yano 1985 model. Allows different concentrations of the
+            nucleotides (as in F81) + distinguishes between transition/transversionmutations
+            (similar to K80). Link:
             Hasegawa, Kishino, Yano (1985), J. Mol. Evol. 22 (2): 160–174. doi:10.1007/BF02101694
-        
+
             Current implementation of the model does not account for the gaps
-        
-            Args: 
-        
-             - mu(float): mutation rate 
-        
+
+            Args:
+
+             - mu(float): mutation rate
+
              - pi(numpy array): nucleotide concentrations
-        
+
              - kappa(float): ratio of transversion/transition mutation rates
 
-        
+
 
          --- T92:
-            Tamura 1992 model. Extending Kimura  (1980) model for the case where a 
+            Tamura 1992 model. Extending Kimura  (1980) model for the case where a
             G+C-content bias exists. Link:
             Tamura K (1992),  Mol.  Biol. Evol. 9 (4): 678–687.  DOI: 10.1093/oxfordjournals.molbev.a040752
-            
-            Current implementation of the model does not account for the gaps
-            
-            Args: 
-             
-             - mu(float): mutation rate 
-        
-             - pi_GC(float): relative GC content 
-        
-             - kappa(float): relative transversion/transition rate 
 
-        
+            Current implementation of the model does not account for the gaps
+
+            Args:
+
+             - mu(float): mutation rate
+
+             - pi_GC(float): relative GC content
+
+             - kappa(float): relative transversion/transition rate
+
+
 
          --- TN93:
-            Tamura and Nei 1993. The model distinguishes between the two different types of 
-            transition: (A <-> G) is allowed to have a different rate to (C<->T). 
-            Transversions have the same rate. The frequencies of the nucleotides are allowed 
+            Tamura and Nei 1993. The model distinguishes between the two different types of
+            transition: (A <-> G) is allowed to have a different rate to (C<->T).
+            Transversions have the same rate. The frequencies of the nucleotides are allowed
             to be different. Link:
             Tamura, Nei (1993), MolBiol Evol. 10 (3): 512–526. DOI:10.1093/oxfordjournals.molbev.a040023
-        
+
             Args:
-        
+
              - mu(float): mutaion rate
-        
-             - kappa1(float): relative A<-->C, A<-->T, T<-->G and G<-->C rates 
-        
-             - kappa2(float): relative C<-->T rate 
-        
+
+             - kappa1(float): relative A<-->C, A<-->T, T<-->G and G<-->C rates
+
+             - kappa2(float): relative C<-->T rate
+
             Note:
-        
-             - Rate of A<-->G mutation is set to one. All other rates (kappa1, kappa2) 
+
+             - Rate of A<-->G mutation is set to one. All other rates (kappa1, kappa2)
             are specified relative to this rate
         """
-        
+
         if model.lower() in ['jc', 'jc69', 'jukes-cantor', 'jukes-cantor69', 'jukescantor', 'jukescantor69']:
             return JC69(**kwargs)
         elif model.lower() in ['k80', 'kimura80', 'kimura1980']:
@@ -469,7 +469,7 @@ class GTR(object):
 
     @classmethod
     def create_random_model(cls, mu=1.0, alphabet='nuc'):
-        
+
         alphabet=alphabets[alphabet]
         gtr = cls(alphabet)
         n = gtr.alphabet.shape[0]
