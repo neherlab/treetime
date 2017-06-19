ee659413360240dd915719c08865918957528166
Richard Neher <richard.neher@unibas.ch>
2017-06-19 16:05:48 +0200

    added small script to exectute test functions manually

---
+++ test/run_tests.py
@@ -0,0 +1,13 @@
+from test_treetime import *
+
+import_short_test()
+
+test_GTR()
+
+test_ancestral()
+
+test_seq_joint_reconstruction_correct()
+
+test_seq_joint_lh_is_max()
+
+print('\n\n TEST HAVE FINISHED SUCCESSFULLY\n\n')
