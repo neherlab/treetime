1436725294235eff4ffc52948329168f73ad40bd
Richard Neher <richard.neher@unibas.ch>
2017-09-06 16:01:59 +0200

    fixed typos

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -258,7 +258,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'TreeTime.tex', u'TreeTime Documentation',
-     u'Prof. Dr. Richard Neher and Pavel Sagulenko', 'manual'),
+     u'Pavel Sagulenko and Richard Neher', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
