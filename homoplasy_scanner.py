494011ec1373cecf85a568508643986a0ce3da99
Richard Neher <richard.neher@unibas.ch>
2018-03-20 10:35:37 +0100

    added comments to homoplasy scanner

--- homoplasy_scanner.py
+++ homoplasy_scanner.py
@@ -16,7 +16,7 @@ if __name__=="__main__":
                         ' might suggest contamination, recombination, culture adaptation or similar. ')
     parser.add_argument('--aln', required = True, type = str,  help ="fasta file with input sequences")
     parser.add_argument('--tree', required = True, type = str,  help ="newick file with tree")
-
+    parser.add_argument('--detailed', required = False, action="store_true",  help ="generate a more detailed report")
     parser.add_argument('--gtr', required=False, type = str, default='infer', help="GTR model to use. "
         " Type 'infer' to infer the model from the data. Or, specify the model type. "
         " If the specified model requires additional options, use '--gtr_args' to specify those")
@@ -76,9 +76,10 @@ if __name__=="__main__":
 
     print("read alignment from file %s with %d sequences of length %d"%(params.aln,N_seq,L))
     print("read tree from file %s with %d leaves"%(params.tree,N_tree))
+    print("\ninferring ancestral sequences...")
 
-    treeanc.infer_ancestral_sequences('ml', infer_gtr=infer_gtr,
-                                       marginal=params.marginal)
+    treeanc.infer_ancestral_sequences('ml', infer_gtr=infer_gtr, marginal=False)
+    print("...done.")
 
     ###########################################################################
     ### analysis of reconstruction
@@ -87,10 +88,10 @@ if __name__=="__main__":
     from scipy.stats import poisson
     offset = 0 if params.zero_based else 1
 
+    # construct dictionaries gathering mutations and positions
     mutations = defaultdict(list)
     positions = defaultdict(list)
     terminal_mutations = defaultdict(list)
-
     for n in treeanc.tree.find_clades():
         if n.up is None:
             continue
@@ -105,6 +106,17 @@ if __name__=="__main__":
                     if '-' not in [a,d]:
                         terminal_mutations[(a,pos+offset,d)].append(n)
 
+    # gather homoplasic mutations by strain
+    mutation_by_strain = defaultdict(list)
+    for n in treeanc.tree.get_terminals():
+        for a,pos,d in n.mutations:
+            if pos in positions and len(positions[pos])>1:
+                mutation_by_strain[n.name].append([(a,pos+offset,d), len(positions[pos])])
+
+
+    # total_branch_length is the expected number of substitutions
+    # corrected_branch_length is the expected number of observable substitutions
+    # (probability of an odd number of substitutions at a particular site)
     total_branch_length = treeanc.tree.total_branch_length()
     corrected_branch_length = np.sum([np.exp(-x.branch_length)*np.sinh(x.branch_length)
                                       for x in treeanc.tree.find_clades()])
@@ -112,37 +124,52 @@ if __name__=="__main__":
                                       for x in treeanc.tree.get_terminals()])
     expected_mutations = L*corrected_branch_length
     expected_terminal_mutations = L*corrected_terminal_branch_length
+
+    # make histograms and sum mutations in different categories
     multiplicities = np.bincount([len(x) for x in mutations.values()])
     total_mutations = np.sum([len(x) for x in mutations.values()])
 
     multiplicities_terminal = np.bincount([len(x) for x in terminal_mutations.values()])
     terminal_mutation_count = np.sum([len(x) for x in terminal_mutations.values()])
 
+    multiplicities_positions = np.bincount([len(x) for x in positions.values()])
+    multiplicities_positions[0] = L - np.sum(multiplicities_positions)
+
+    ###########################################################################
+    ### Output the distribution of times particular mutations are observed
+    ###########################################################################
     print("\nThe TOTAL tree length is %1.3e, expecting %1.1f mutations vs an observed %d"
           %(total_branch_length,expected_mutations,total_mutations))
     print("Of these %d mutations,"%total_mutations
             +"".join(['\n\t - %d occur %d times'%(n,mi)
                       for mi,n in enumerate(multiplicities) if n]))
-    print("\nThe TERMINAL branch length is %1.3e, expecting %1.1f mutations vs an observed %d"
-          %(corrected_terminal_branch_length,expected_terminal_mutations,terminal_mutation_count))
-    print("Of these %d mutations,"%terminal_mutation_count
-            +"".join(['\n\t - %d occur %d times'%(n,mi)
-                      for mi,n in enumerate(multiplicities_terminal) if n]))
+    # additional optional output this for terminal mutations only
+    if params.detailed:
+        print("\nThe TERMINAL branch length is %1.3e, expecting %1.1f mutations vs an observed %d"
+              %(corrected_terminal_branch_length,expected_terminal_mutations,terminal_mutation_count))
+        print("Of these %d mutations,"%terminal_mutation_count
+                +"".join(['\n\t - %d occur %d times'%(n,mi)
+                          for mi,n in enumerate(multiplicities_terminal) if n]))
 
 
-    multiplicities_positions = np.bincount([len(x) for x in positions.values()])
-    multiplicities_positions[0] = L - np.sum(multiplicities_positions)
+    ###########################################################################
+    ### Output the distribution of times mutations at particular positions are observed
+    ###########################################################################
     print("\nOf the %d positions in the genome,"%L
             +"".join(['\n\t - %d were hit %d times (expected %1.2f)'%(n,mi,L*poisson.pmf(mi,corrected_branch_length))
                       for mi,n in enumerate(multiplicities_positions) if n]))
 
 
+    # compare that distribution to a Poisson distribution with the same mean
     p = poisson.pmf(np.arange(10*multiplicities_positions.max()),1.0*total_mutations/L)
     print("\nlog-likelihood difference to Poisson distribution with same mean: %1.3e"%(
             - L*np.sum(p*np.log(p+1e-100))
             + np.sum(multiplicities_positions*np.log(p[:len(multiplicities_positions)]+1e-100))))
 
 
+    ###########################################################################
+    ### Output the mutations that are observed most often
+    ###########################################################################
     print("\n\nThe ten most homoplasic mutations are:\n\tmut\tmultiplicity")
     mutations_sorted = sorted(mutations.items(), key=lambda x:len(x[1])-0.1*x[0][1]/L, reverse=True)
     for mut, val in mutations_sorted[:10]:
@@ -151,11 +178,22 @@ if __name__=="__main__":
         else:
             break
 
-    print("\n\nThe ten most homoplasic mutation on terminal branches are:\n\tmut\tmultiplicity")
-    terminal_mutations_sorted = sorted(terminal_mutations.items(), key=lambda x:len(x[1])-0.1*x[0][1]/L, reverse=True)
-    for mut, val in terminal_mutations_sorted[:10]:
-        if len(val)>1:
-            print("\t%s%d%s\t%d"%(mut[0], mut[1], mut[2], len(val)))
-        else:
-            break
+    # optional output specifically for mutations on terminal branches
+    if params.detailed:
+        print("\n\nThe ten most homoplasic mutation on terminal branches are:\n\tmut\tmultiplicity")
+        terminal_mutations_sorted = sorted(terminal_mutations.items(), key=lambda x:len(x[1])-0.1*x[0][1]/L, reverse=True)
+        for mut, val in terminal_mutations_sorted[:10]:
+            if len(val)>1:
+                print("\t%s%d%s\t%d"%(mut[0], mut[1], mut[2], len(val)))
+            else:
+                break
 
+    ###########################################################################
+    ### Output strains that have many homoplasic mutations
+    ###########################################################################
+    # TODO: add statistical criterion
+    if params.detailed:
+        print("\n\nTaxons that carry positions that mutated elsewhere in the tree:\n\ttaxon name\t#of homoplasic mutations")
+        mutation_by_strain_sorted = sorted(mutation_by_strain.items(), key=lambda x:len(x[1]), reverse=True)
+        for name, val in mutation_by_strain_sorted[:10]:
+            print("\t%s\t%d"%(name, len(val)))
