181b025628c8407f1dd676a9b264cc6d10e4bd3c
Richard Neher <richard.neher@unibas.ch>
2019-06-20 17:45:48 +0200

    conditional python version

--- setup.py
+++ setup.py
@@ -27,10 +27,13 @@ setup(
         packages=['treetime'],
         install_requires = [
             'biopython>=1.66',
-            'matplotlib>=2.0',
             'numpy>=1.10.4',
             'pandas>=0.17.1',
             'scipy>=0.16.1'
+        ],
+        extras_require = [
+            ':python_version < "3.6"':['matplotlib>=2.0, ==2.*'],
+            ':python_version >= "3.6"':['matplotlib>=2.0'],
         ],
         classifiers=[
             "Development Status :: 3 - Alpha",
