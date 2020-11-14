6e627e7c3b6e12ba2e13457353abbc7010ac4300
Richard Neher <richard.neher@unibas.ch>
2020-11-15 00:06:27 +0100

    adjust readme and setup description to reflect version support change

--- setup.py
+++ setup.py
@@ -39,9 +39,10 @@ setup(
             "Development Status :: 3 - Alpha",
             "Topic :: Scientific/Engineering :: Bio-Informatics",
             "License :: OSI Approved :: MIT License",
-            "Programming Language :: Python :: 2.7",
             "Programming Language :: Python :: 3.5",
-            "Programming Language :: Python :: 3.6"
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8"
             ],
         scripts=['bin/treetime']
     )
