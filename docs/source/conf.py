9611672f7a453f80c2f7b412d2d745f700e46127
Richard Neher <richard.neher@unibas.ch>
2021-10-26 21:49:02 +0200

    re-activate rtd theme:

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -130,7 +130,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'alabaster'
+# html_theme = 'alabaster'
 
 html_theme = 'sphinx_rtd_theme'
 
