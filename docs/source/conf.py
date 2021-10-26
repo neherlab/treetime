5d4c15db5886b9a02b26321b3f984938712f3b90
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-10-26 23:23:03 +0200

    docs: reconfigure the docs build system

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,11 +14,13 @@
 
 import sys
 import os
-from treetime import version
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../..'))
+
+from treetime import version
 
 # -- General configuration ------------------------------------------------
 
