50c5b42b68692ca7a78024adbb9c2700316afa29
Richard Neher <richard.neher@unibas.ch>
2022-05-06 17:56:50 +0200

    fix: pass rerooting argument to ARG initialization

--- treetime/wrappers.py
+++ treetime/wrappers.py
@@ -489,13 +489,15 @@ def arg_time_trees(params):
                     fill_overhangs=not params.keep_overhangs)
 
     dates = utils.parse_dates(params.dates, date_col=params.date_column, name_col=params.name_column)
+    root = None if params.keep_root else params.reroot
+
     for i,(tree,mask) in enumerate(zip(arg_params['trees'], arg_params['masks'])):
         outdir = get_outdir(params, f'_ARG-treetime')
         gtr = create_gtr(params)
 
         tt = setup_arg(tree, arg_params['alignment'], arg_params['combined_mask'], mask, dates, arg_params['MCCs'],
-                       gtr=gtr, verbose=params.verbose, fill_overhangs=not params.keep_overhangs, 
-                       fixed_clock_rate = params.clock_rate)
+                       gtr=gtr, verbose=params.verbose, fill_overhangs=not params.keep_overhangs,
+                       fixed_clock_rate = params.clock_rate, reroot=root)
 
         run_timetree(tt, params, outdir, tree_suffix=f"_{i+1}", prune_short=False, method_anc=params.method_anc)
 
@@ -644,7 +646,7 @@ def run_timetree(myTree, params, outdir, tree_suffix='', prune_short=True, metho
             print(" --N_e: \t %1.2e \tcorresponding 'effective population size' assuming 50 gen/year\n"%(Tc/myTree.date2dist.clock_rate*50))
 
     # plot
-    ##IMPORTANT: after this point the functions not only plot the tree but also modify the branch length 
+    ##IMPORTANT: after this point the functions not only plot the tree but also modify the branch length
     import matplotlib.pyplot as plt
     from .treetime import plot_vs_years
     leaf_count = myTree.tree.count_terminals()
