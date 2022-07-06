9dc7f96b3b76096ca0d77f4a3b65750a2f7a386f
ivan-aksamentov <ivan.aksamentov@gmail.com>
2022-07-06 05:10:08 +0200

    feat: restrict access to children/parents during graph traversal

--- packages/treetime/src/bin/graph.rs
+++ packages/treetime/src/bin/graph.rs
@@ -1,6 +1,7 @@
 use ctor::ctor;
 use eyre::Report;
-use treetime::graph::graph::{Graph, GraphNode};
+use itertools::Itertools;
+use treetime::graph::graph::Graph;
 use treetime::utils::global_init::global_init;
 
 #[cfg(all(target_family = "linux", target_arch = "x86_64"))]
@@ -12,47 +13,65 @@ fn init() {
   global_init();
 }
 
+/// An example of node payload type
 #[derive(Clone, Debug)]
 pub struct NodePayload {
-  data: &'static str,
+  name: &'static str,
 }
 
 fn main() -> Result<(), Report> {
   let mut graph = Graph::new();
-  let root1 = graph.insert(NodePayload { data: "root1" });
-  let root2 = graph.insert(NodePayload { data: "root2" });
-  let a = graph.insert(NodePayload { data: "a" });
-  let b = graph.insert(NodePayload { data: "b" });
-  let c = graph.insert(NodePayload { data: "c" });
-  let d = graph.insert(NodePayload { data: "d" });
-  let e = graph.insert(NodePayload { data: "e" });
-  let f = graph.insert(NodePayload { data: "f" });
-  let g = graph.insert(NodePayload { data: "g" });
-  let h = graph.insert(NodePayload { data: "h" });
 
+  // Create nodes of the graph by providing their payloads. Node payload can be any arbitrary type.
+  // In this case it is a `struct NodePayload` defined just above.
+  //
+  // At this point, nodes are not connected yet. Each insertion operation returns an index of
+  // a newly created node, which can later be used for creating graph edges.
+  let root1 = graph.insert(NodePayload { name: "root1" });
+  let root2 = graph.insert(NodePayload { name: "root2" });
+  let a = graph.insert(NodePayload { name: "a" });
+  let b = graph.insert(NodePayload { name: "b" });
+  let c = graph.insert(NodePayload { name: "c" });
+  let d = graph.insert(NodePayload { name: "d" });
+  let e = graph.insert(NodePayload { name: "e" });
+  let f = graph.insert(NodePayload { name: "f" });
+  let g = graph.insert(NodePayload { name: "g" });
+  let h = graph.insert(NodePayload { name: "h" });
+
+  // Connect nodes pairwise. Each connection operation creates a graph edge between a pair of nodes.
+  // The edge is directed from the first node to the second node, i.e. the first node is considered
+  // a parent and the second node is considered a child.
   graph.connect(root1, a);
   graph.connect(root2, b);
-
   graph.connect(a, c);
   graph.connect(a, f);
-
   graph.connect(a, d);
   graph.connect(b, d);
   graph.connect(d, g);
-
   graph.connect(b, e);
   graph.connect(e, h);
 
+  // Finalize the graph. Performs some internal bookkeeping.
   graph.build();
 
   println!("Traverse forward:");
-  graph.iter_breadth_first(|node| {
-    println!("{}", &node.payload.data);
+  println!("{:^6} | {:^20} | {:^}", "Node", "Parents", "Is root");
+  graph.iter_breadth_first_forward(|node| {
+    let node_data = &node.payload.name;
+    let parent_names = &node.parents.iter().map(|parent| &parent.name).join(", ");
+    let is_root = &node.is_root();
+    println!("{node_data:>6} | {parent_names:20} | {is_root}");
   });
 
+  println!();
+
   println!("Traverse backwards:");
+  println!("{:^6} | {:^20} | {:^}", "Node", "Parents", "Is leaf");
   graph.iter_breadth_first_reverse(|node| {
-    println!("{}", &node.payload.data);
+    let node_name = &node.payload.name;
+    let child_names = &node.children.iter().map(|child| &child.name).join(", ");
+    let is_leaf = &node.is_leaf();
+    println!("{node_name:>6} | {child_names:20} | {is_leaf}");
   });
 
   Ok(())
--- packages/treetime/src/graph/graph.rs
+++ packages/treetime/src/graph/graph.rs
@@ -1,15 +1,17 @@
+use itertools::Itertools;
 use std::collections::{HashSet, VecDeque};
 
+/// Internal representation of a node
 #[derive(Clone, Debug)]
-pub struct GraphNode<N> {
+struct GraphNodeImpl<N> {
   idx: usize,
   parents: Vec<usize>,
   children: Vec<usize>,
   visited: bool,
-  pub payload: N,
+  payload: N,
 }
 
-impl<N> GraphNode<N> {
+impl<N> GraphNodeImpl<N> {
   pub const fn new(idx: usize, payload: N) -> Self {
     Self {
       idx,
@@ -29,11 +31,44 @@ impl<N> GraphNode<N> {
   }
 }
 
+/// Public representation of a node during forward traversal (from roots to leaves).
+///
+/// Here we only have access to parent data, because child data is not yet computed.
+#[derive(Clone, Debug)]
+pub struct GraphNodeForward<'node, N> {
+  pub idx: usize,
+  pub parents: Vec<&'node N>,
+  pub payload: &'node N,
+}
+
+impl<'node, N> GraphNodeForward<'node, N> {
+  pub fn is_root(&self) -> bool {
+    self.parents.is_empty()
+  }
+}
+
+/// Public representation of a node during reverse traversal
+///
+/// Here we only have access to child data, because parent data is not yet computed.
+#[derive(Clone, Debug)]
+pub struct GraphNodeReverse<'node, N> {
+  pub idx: usize,
+  pub children: Vec<&'node N>,
+  pub payload: &'node N,
+}
+
+impl<'node, N> GraphNodeReverse<'node, N> {
+  pub fn is_leaf(&self) -> bool {
+    self.children.is_empty()
+  }
+}
+
+/// Representation of a graph
 #[derive(Clone, Debug)]
 pub struct Graph<N> {
-  pub nodes: Vec<GraphNode<N>>,
-  pub roots: Vec<usize>,
-  pub leaves: Vec<usize>,
+  nodes: Vec<GraphNodeImpl<N>>,
+  roots: Vec<usize>,
+  leaves: Vec<usize>,
 }
 
 impl<N> Graph<N> {
@@ -47,13 +82,19 @@ impl<N> Graph<N> {
   }
 
   /// Adds a node, given its payload. Returns index of the added node.
+  ///
+  /// The order of insertion can be arbitrary and is not taken into account in any of the graph
+  /// operations.
   pub fn insert(&mut self, node_payload: N) -> usize {
-    let node = GraphNode::new(self.nodes.len(), node_payload);
+    let node = GraphNodeImpl::new(self.nodes.len(), node_payload);
     self.nodes.push(node);
     self.nodes.len() - 1
   }
 
   /// Adds an edge between two nodes given their indices
+  ///
+  /// The edge is directed from the first node to the second node, i.e. the first node is considered
+  /// a parent and the second node is considered a child.
   pub fn connect(&mut self, from_idx: usize, to_idx: usize) {
     let from = &mut self.nodes[from_idx];
     from.children.push(to_idx);
@@ -75,29 +116,47 @@ impl<N> Graph<N> {
     self
   }
 
-  /// Iterates graph from roots to leaves in breadth-first order
-  pub fn iter_breadth_first(&self, f: impl Fn(&GraphNode<N>)) {
+  /// Iterates graph from roots to leaves in breadth-first order.
+  ///
+  /// Guarantees that for each visited node, all of it parents (recursively) are visited before
+  /// the node itself is visited.
+  ///
+  /// The order of traversal does not take into account the order of insertion of nodes or edges.
+  pub fn iter_breadth_first_forward(&self, visitor_func: impl Fn(&GraphNodeForward<N>)) {
     let mut queue: VecDeque<usize> = self.roots.iter().copied().collect();
     let mut visited = HashSet::<usize>::new();
     while let Some(idx) = queue.pop_front() {
       if !visited.contains(&idx) {
         let node = &self.nodes[idx];
         queue.extend(node.children.iter().copied());
-        f(node);
+        visitor_func(&GraphNodeForward {
+          idx,
+          parents: node.parents.iter().map(|&idx| &self.nodes[idx].payload).collect_vec(),
+          payload: &node.payload,
+        });
         visited.insert(idx);
       }
     }
   }
 
   /// Iterates graph from leaves to roots in breadth-first order
-  pub fn iter_breadth_first_reverse(&self, f: impl Fn(&GraphNode<N>)) {
+  ///
+  /// Guarantees that for each visited node, all of it children (recursively) are visited before
+  /// the node itself is visited.
+  ///
+  /// The order of traversal does not take into account the order of insertion of nodes or edges.
+  pub fn iter_breadth_first_reverse(&self, visitor_func: impl Fn(&GraphNodeReverse<N>)) {
     let mut queue: VecDeque<usize> = self.leaves.iter().copied().collect();
     let mut visited = HashSet::<usize>::new();
     while let Some(idx) = queue.pop_front() {
       if !visited.contains(&idx) {
         let node = &self.nodes[idx];
         queue.extend(node.parents.iter().copied());
-        f(node);
+        visitor_func(&GraphNodeReverse {
+          idx,
+          children: node.children.iter().map(|&idx| &self.nodes[idx].payload).collect_vec(),
+          payload: &node.payload,
+        });
         visited.insert(idx);
       }
     }
