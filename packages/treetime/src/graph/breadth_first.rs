17365fe892909280065b015dc7af79f07112d733
ivan-aksamentov <ivan.aksamentov@gmail.com>
2022-07-14 11:00:12 +0200

    fix: avoid data race when filtering frontier candidates in the graph

--- packages/treetime/src/graph/breadth_first.rs
+++ packages/treetime/src/graph/breadth_first.rs
@@ -1,7 +1,6 @@
 use crate::graph::node::Node;
-use itertools::Itertools;
 use parking_lot::{RwLock, RwLockWriteGuard};
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use rayon::iter::{IntoParallelIterator, IntoParallelRefIterator, ParallelIterator};
 use std::fmt::{Debug, Display};
 use std::hash::Hash;
 use std::sync::Arc;
@@ -12,50 +11,61 @@ where
   E: Clone + Debug + Display + Sync + Send,
   F: Fn(&RwLockWriteGuard<Node<N, E>>) + Sync + Send,
 {
-  // Walk the graph one "frontier" at a time. Frontier is a set of nodes of a "level" in the graph, where each node
+  // Walk the graph one "frontier" at a time. Frontier is a set of nodes of a "layer" in the graph, where each node
   // has its dependencies already resolved. Frontiers allow parallelism.
   let mut frontier = sources.to_vec();
 
+  // We traverse the graph, gathering frontiers. The last frontier will be empty (nodes of the previous to last
+  // frontier will have no unvisited children), ending this loop.
   while !frontier.is_empty() {
     // Process each node in the current frontier concurrently
-    frontier = frontier
+    let frontier_candidates: Vec<Arc<RwLock<Node<N, E>>>> = frontier
       .into_par_iter()
       .map(|node| {
         {
           let mut node = node.write();
+          if !node.is_visited() {
+            // The actual visit. Here we call the user-provided function.
+            explorer(&node);
 
-          // The actual visit. Here we call the user-provided function.
-          explorer(&node);
-
-          // We mark the node as visited so that it's not visited twice and telling following loop iterations
-          // that its children can potentially be processed now
-          node.mark_as_visited();
+            // We mark the node as visited so that it's not visited twice and telling following loop iterations
+            // that its children can potentially be processed now
+            node.mark_as_visited();
+          }
         }
 
-        // Here we iterate over node's children and decide which children to add to the next frontier:
-        // We only queue the children which have ALL of its parents already visited.
-        // This ensures "lazy", "exploration" traversal algorithm, where each node is visited exactly once
-        // (as opposed to "greedy", "search-like" or "shortest path" algorithm where some of the nodes can be skipped
-        // if they are not in the shortest path)
+        // Gather node's children
         let node = node.read();
-        let children = node.outbound()
-          .iter()
+        let children: Vec<Arc<RwLock<Node<N, E>>>> = node.outbound()
+          .par_iter()
           .map(|edge| edge.target())
-          .filter(|child| {
-            child
-              .read()
-              .inbound()
-              .iter()
-              .filter_map(|edge| edge.upgrade().map(|edge| Arc::clone(&edge.source())))
-              .all(|childs_parent| childs_parent.read().is_visited())
-          })
-          .collect_vec();
+          .collect();
 
         children
       })
-        // For each node's iteration, we receive a list of its children, so overall a list of lists.
-        // We flatten anc collect it into a new frontier.
+        // For each node, we receive a list of its children, so overall a list of lists. We flatten it here into a
+        // flat list.
       .flatten()
       .collect();
+
+    // NOTE: this is a barrier. The separation of the two loops is necessary for synchronization.
+
+    // Decide which children to add to the next frontier. We only add the children which have ALL of its parents
+    // already visited. This ensures exploration where each node is visited exactly once and only when all of its
+    // dependencies are resolved.
+    let next_frontier = frontier_candidates
+      .into_par_iter()
+      .filter(|child| {
+        child
+          .read()
+          .inbound()
+          .iter()
+          .filter_map(|edge| edge.upgrade().map(|edge| Arc::clone(&edge.source())))
+          .all(|childs_parent| childs_parent.read().is_visited())
+      })
+      .collect();
+
+    // The newly gathered frontier becomes the new current frontier
+    frontier = next_frontier;
   }
 }
