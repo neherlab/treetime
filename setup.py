40efb51b9483bc4e0f9fb363869ff9a8e9cdf358
Cornelius Roemer <cornelius.roemer@gmail.com>
2022-08-02 22:56:40 +0200

    chore: update supported versions in setup.py classifiers
    deprecate 3.5
    add support for 3.9 and 3.10

--- setup.py
+++ setup.py
@@ -39,10 +39,11 @@ setup(
             "Development Status :: 5 - Production/Stable",
             "Topic :: Scientific/Engineering :: Bio-Informatics",
             "License :: OSI Approved :: MIT License",
-            "Programming Language :: Python :: 3.5",
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
             ],
         entry_points = {
             "console_scripts": [
