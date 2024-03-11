be03d95d0cdfb4fd9119c185eb721a208d4f9788
ivan-aksamentov <ivan.aksamentov@gmail.com>
2024-03-11 09:55:43 +0100

    test: port gtr_tests.py

--- packages/treetime/src/utils/assert.rs
+++ packages/treetime/src/utils/assert.rs
@@ -2,8 +2,43 @@
 macro_rules! pretty_assert_eq {
   ($left:expr, $right:expr) => {{
     pretty_assertions::assert_eq!(
-      format!("{:#?}", $left).replace("\n", "\u{0085}"),
-      format!("{:#?}", $right).replace("\n", "\u{0085}")
+      format_newlines(format!("{:#?}", $left))
+      format_newlines(format!("{:#?}", $right))
     );
   }};
+  ($left:expr, $right:expr,) => {{
+    pretty_assertions::assert_eq!(
+      format_newlines(format!("{:#?}", $left))
+      format_newlines(format!("{:#?}", $right))
+    );
+  }};
+}
+
+#[macro_export]
+macro_rules! pretty_assert_ulps_eq {
+  ($lhs:expr, $rhs:expr $(, $opt:ident = $val:expr)*) => {{
+    if ! approx::ulps_eq!($lhs, $rhs, $($opt = $val,)*) {
+      pretty_assertions::assert_eq!(
+        $crate::utils::assert::format_array(format!("{:#?}", $lhs)),
+        $crate::utils::assert::format_array(format!("{:#?}", $rhs)),
+      );
+    }
+  }};
+  ($lhs:expr, $rhs:expr $(, $opt:ident = $val:expr)*,) => {{
+    if ! approx::ulps_eq!($lhs, $rhs, $($opt = $val,)*) {
+      pretty_assertions::assert_eq!(
+        $crate::utils::assert::format_array(format!("{:#?}", $lhs)),
+        $crate::utils::assert::format_array(format!("{:#?}", $rhs)),
+      );
+    }
+  }};
+}
+
+pub fn format_newlines(s: impl AsRef<str>) -> String {
+  s.as_ref().replace('\n', "\u{0085}")
+}
+
+pub fn format_array(s: impl AsRef<str>) -> String {
+  let re = regex::Regex::new(r", shape=.*").unwrap();
+  re.replace(&format_newlines(s), "").into_owned()
 }
