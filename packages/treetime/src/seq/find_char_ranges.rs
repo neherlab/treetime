707dbe61ca8b962135ec3535a66018c94a26f099
ivan-aksamentov <ivan.aksamentov@gmail.com>
2024-03-19 23:08:24 +0100

    feat: add find_undetermined_ranges(), tests

--- packages/treetime/src/seq/find_char_ranges.rs
+++ packages/treetime/src/seq/find_char_ranges.rs
@@ -1,3 +1,5 @@
+use crate::seq::range_union::range_union;
+
 // Finds contiguous ranges (segments) in the sequence, such that for every character inside every range,
 // the predicate function returns true and every range contains only the same letter.
 //
@@ -58,4 +60,75 @@ pub fn find_ambiguous_ranges(seq: &[char]) -> Vec<(usize, usize)> {
 
 pub fn find_gap_ranges(seq: &[char]) -> Vec<(usize, usize)> {
   find_letter_ranges(seq, '-')
+}
+
+pub fn find_undetermined_ranges(seq: &[char]) -> Vec<(usize, usize)> {
+  range_union(&[find_letter_ranges_by(seq, |c| c == 'N' || c == '-')])
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use rstest::rstest;
+
+  fn to_char_array(seq: &str) -> Vec<char> {
+    seq.chars().collect()
+  }
+
+  #[rstest]
+  #[case("",                'X', vec![])]
+  #[case("GGATNNACA-ANTYGG",'X', vec![])]
+  #[case("XXX",             'X', vec![(0, 3)])]
+  #[case("ATGXXXTTTT",      'X', vec![(3, 6)])]
+  #[case("ATGXXXCATGXXXXA", 'X', vec![(3, 6), (10, 14)])]
+  #[case("GCAXXXX",         'X', vec![(3, 7)])]
+  #[case("XXXXGCA",         'X', vec![(0, 4)])]
+  fn test_find_letter_ranges(#[case] seq: &str, #[case] letter: char, #[case] expected: Vec<(usize, usize)>) {
+    let actual = find_letter_ranges(&to_char_array(seq), letter);
+    assert_eq!(expected, actual);
+  }
+
+  #[rstest]
+  #[case("",                vec![])]
+  #[case("GGATGCACATATTAGG",vec![])]
+  #[case("NNN",             vec![(0, 3)])]
+  #[case("ATGNNNTTTT",      vec![(3, 6)])]
+  #[case("ATGNNNCATGNNNNA", vec![(3, 6), (10, 14)])]
+  #[case("GCANNNN",         vec![(3, 7)])]
+  #[case("NNNNGCA",         vec![(0, 4)])]
+  fn test_find_ambiguous_ranges(#[case] seq: &str, #[case] expected: Vec<(usize, usize)>) {
+    let actual = find_ambiguous_ranges(&to_char_array(seq));
+    assert_eq!(expected, actual);
+  }
+
+  #[rstest]
+  #[case("",                vec![])]
+  #[case("GGATGCACATATTAGG",vec![])]
+  #[case("---",             vec![(0, 3)])]
+  #[case("ATG---TTTT",      vec![(3, 6)])]
+  #[case("ATG---CATG----A", vec![(3, 6), (10, 14)])]
+  #[case("GCA----",         vec![(3, 7)])]
+  #[case("----GCA",         vec![(0, 4)])]
+  fn test_find_gap_ranges(#[case] seq: &str, #[case] expected: Vec<(usize, usize)>) {
+    let actual = find_gap_ranges(&to_char_array(seq));
+    assert_eq!(expected, actual);
+  }
+
+  #[rstest]
+  #[case("",                 vec![])]
+  #[case("GGATGCACATATTAGG", vec![])]
+  #[case("NNNN",             vec![(0, 4)])]
+  #[case("----",             vec![(0, 4)])]
+  #[case("NNNN----",         vec![(0, 8)])]
+  #[case("----NNNN",         vec![(0, 8)])]
+  #[case("----NNNN----",     vec![(0, 12)])]
+  #[case("NNNN----NNNN",     vec![(0, 12)])]
+  #[case("A----TNNNNG----C", vec![(1, 5), (6, 10), (11, 15)])]
+  #[case("ANNNNT----GNNNNC", vec![(1, 5), (6, 10), (11, 15)])]
+  #[case("ATGNNNTTTT---",    vec![(3, 6), (10, 13)])]
+  #[case("ATG---TTTTNNN",    vec![(3, 6), (10, 13)])]
+  fn test_find_undetermined_ranges(#[case] seq: &str, #[case] expected: Vec<(usize, usize)>) {
+    let actual = find_undetermined_ranges(&to_char_array(seq));
+    assert_eq!(expected, actual);
+  }
 }
