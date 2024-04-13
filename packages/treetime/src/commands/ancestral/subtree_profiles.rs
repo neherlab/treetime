c3955813a331354371346524a93ba8a8148d8300
ivan-aksamentov <ivan.aksamentov@gmail.com>
2024-04-13 02:38:30 +0200

    test: remove duplicated test step from subtree_profile
    The same test for sequence reconstruction is in anc_reconstruction_marginal

--- packages/treetime/src/commands/ancestral/subtree_profiles.rs
+++ packages/treetime/src/commands/ancestral/subtree_profiles.rs
@@ -224,31 +224,7 @@ mod tests {
 
     pretty_assert_ulps_eq!(-57.189205994979055, logLH, epsilon = 1e-5);
 
-    let mut actual = btreemap! {};
-    pre_order_intrusive(&graph, &root, &mut root_seq, &mut |node, seq| {
-      let node = node.write_arc().payload().write_arc();
-      let mut seq = seq.to_owned();
-      for (&pos, vec) in &node.profile_variable {
-        seq[pos] = gtr.alphabet.char(vec.argmax().unwrap());
-      }
-      actual.insert(node.name.clone(), vec_to_string(seq));
-    });
 
-    #[rustfmt::skip]
-    let expected = BTreeMap::from([
-      (o!("A"),    o!("ACATCGCCGTATTG")),
-      (o!("AB"),   o!("ACATCGCTGTATTG")),
-      (o!("B"),    o!("GCATCCCTGTATTG")),
-      (o!("C"),    o!("CCGGCGATGTATTG")),
-      (o!("CD"),   o!("TCGGCGGTGTATTG")),
-      (o!("D"),    o!("TCGGCCGTGTATTG")),
-      (o!("root"), o!("TCGGCGCTGTATTG")),
-    ]);
-
-    assert_eq!(
-      json_stringify(&expected, JsonPretty(false))?,
-      json_stringify(&actual, JsonPretty(false))?
-    );
 
     Ok(())
   }
