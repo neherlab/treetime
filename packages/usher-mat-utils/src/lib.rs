a8f04ea36af466521ee513709c9e5fc013e976c0
ivan-aksamentov <ivan.aksamentov@gmail.com>
2024-07-21 10:27:33 +0200

    feat: add protobuf schema and codegen for usher mat format

---
+++ packages/usher-mat-utils/src/lib.rs
@@ -0,0 +1,9 @@
+pub mod mutation_detailed {
+  include!(concat!(env!("OUT_DIR"), "/mutation_detailed.rs"));
+}
+pub mod parsimony {
+  include!(concat!(env!("OUT_DIR"), "/parsimony.rs"));
+}
+pub mod taxodium {
+  include!(concat!(env!("OUT_DIR"), "/taxodium.rs"));
+}
