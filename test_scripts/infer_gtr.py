c4207c8824195fa80e3072130337fa660d614757
Richard Neher <richard.neher@unibas.ch>
2024-08-20 21:31:12 +0200

    feat-li: add function to gather mutation counts

--- test_scripts/infer_gtr.py
+++ test_scripts/infer_gtr.py
@@ -1,4 +1,12 @@
 import numpy as np
+from Bio import AlignIO
+import numpy as np
+from lib import Graph, graph_from_nwk_str, GraphNodeBackward, GraphNodeForward
+from lib import SparseSeqDis, VarPos
+from treetime import GTR
+from payload import NodePayload, EdgePayload
+from profile_map import profile_map
+from fitch import init_sequences_sparse
 
 
 def avg_transition(W, pi) -> np.float64:
@@ -8,6 +16,28 @@ def avg_transition(W, pi) -> np.float64:
 def distance(pi_old, pi) -> np.float64:
     #return sum([(x_i - y_i) ** 2 for x_i, y_i in zip(pi_old, pi)]) ** 0.5
     return np.sum((pi_old-pi)**2)**0.5  # numpy version ;)
+
+
+def get_mutation_counts(graph):
+    root = graph.get_one_root().payload()
+    nij = np.zeros((4, 4))
+    Ti = np.zeros(4)
+    seq = root.sparse_sequences[0].seq
+    root_state = np.array([seq.composition[nuc] for nuc in 'ACGT'])
+    char_to_index = {nuc: i for i, nuc in enumerate('ACGT')}
+    for e in graph.get_edges():
+        target_seq = graph.get_node(e.target()).payload().sparse_sequences[0].seq
+        bl = e.payload().branch_length
+        for i, nuc in enumerate('ACGT'):
+            Ti[i] += bl * target_seq.composition[nuc]
+
+        for mut in e.payload().sparse_sequences[0].muts:
+            i, j = char_to_index[mut.qry], char_to_index[mut.ref]
+            nij[i,j] += 1
+            Ti[i] -= 0.5*bl
+            Ti[j] += 0.5*bl
+
+    return nij, Ti, root_state
 
 
 def infer_gtr(
@@ -103,8 +133,26 @@ def infer_gtr(
     return {"W": W_ij, "pi": pi, "mu": mu}
 
 
-if __name__ == "__main__":
+def test():
     nij = np.array([[0, 1, 2, 1], [1, 0, 3, 2], [2, 3, 0, 1], [2, 3, 3, 0]])
     Ti = np.array([12.0, 20., 14.0, 12.4])
     root_state = np.array([3, 2, 3, 4])
+    print(infer_gtr(nij, Ti, root_state, pc=0.1))
+
+if __name__=="__main__":
+    fname_nwk = 'data/ebola/ebola.nwk'
+    fname_seq = 'data/ebola/ebola_dna.fasta'
+    fname_nwk = 'test_scripts/data/tree.nwk'
+    fname_seq = 'test_scripts/data/sequences.fasta'
+    with open(fname_nwk) as fh:
+        nwkstr = fh.read()
+    G = graph_from_nwk_str(nwk_string=nwkstr, node_payload_factory=NodePayload, edge_payload_factory=EdgePayload)
+
+    aln = {seq.id: str(seq.seq).upper() for seq in AlignIO.read(fname_seq, 'fasta')}
+    gtr = GTR.custom(pi=[0.2, 0.3, 0.15, 0.35], alphabet='nuc_nogap')
+    init_sequences_sparse(G, [aln], [gtr])
+
+
+    nij, Ti, root_state = get_mutation_counts(G)
+
     print(infer_gtr(nij, Ti, root_state, pc=0.1))
