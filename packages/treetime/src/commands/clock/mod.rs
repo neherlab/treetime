a09bce69137f2622c886cb0d570df1794b6eb88c
ivan-aksamentov <ivan.aksamentov@gmail.com>
2024-08-27 22:41:26 +0200

    reractor: remove old code

--- packages/treetime/src/commands/clock/mod.rs
+++ packages/treetime/src/commands/clock/mod.rs
@@ -1,9 +1,3 @@
 pub mod clock_args;
-pub mod clock_graph;
-pub mod graph_regression;
-pub mod graph_regression_policy;
 pub mod minimize_scalar;
-pub mod prune_short_branches;
 pub mod run_clock;
-pub mod run_clock_model;
-pub mod run_reroot;
