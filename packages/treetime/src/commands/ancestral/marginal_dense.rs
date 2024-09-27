cc72edc471e6d278f875caca262573821be4ebdc
Richard Neher <richard.neher@unibas.ch>
2024-09-27 23:58:39 +0200

    tests: add tests to marginal_dense

--- packages/treetime/src/commands/ancestral/marginal_dense.rs
+++ packages/treetime/src/commands/ancestral/marginal_dense.rs
@@ -9,7 +9,7 @@ use crate::representation::partitions_likelihood::{PartitionLikelihood, Partitio
 use crate::utils::container::get_exactly_one_mut;
 use crate::utils::interval::range_intersection::range_intersection;
 use crate::utils::ndarray::log;
-use crate::{make_internal_report, make_report, seq};
+use crate::{make_internal_report, make_report};
 use eyre::Report;
 use itertools::Itertools;
 use ndarray::prelude::*;
@@ -133,8 +133,11 @@ fn ingroup_profiles_dense(graph: &DenseGraph, partitions: &[PartitionLikelihood]
 
       if node.is_root {
         let seq_info = &mut node.payload.dense_partitions[si];
-        seq_info.profile.dis = &msg_to_parent.dis * &gtr.pi;
-        seq_info.profile.log_lh = msg_to_parent.log_lh;
+        let dis = &msg_to_parent.dis * &gtr.pi;
+        let norm = dis.sum_axis(Axis(1));
+
+        seq_info.profile.dis = (&dis.t() / &norm).t().to_owned();
+        seq_info.profile.log_lh = msg_to_parent.log_lh + log(&norm).sum();
       } else {
         // what was calculated above is what is sent to the parent. we also calculate the propagated message to the parent (we need it in the forward pass).
         let edge_to_parent =
@@ -181,7 +184,8 @@ fn outgroup_profiles_dense(graph: &DenseGraph, partitions: &[PartitionLikelihood
         let mut log_lh = 0.0;
         for (pi, (p, edge)) in node.parents.iter().enumerate() {
           let edge = edge.read_arc();
-          let expQt = gtr.expQt(edge.branch_length.unwrap_or(0.0));
+          let expQt_matrix = gtr.expQt(edge.branch_length.unwrap_or(0.0));
+          let expQt = expQt_matrix.t();
           msgs_to_combine.push(edge.dense_partitions[si].msg_to_parent.dis.view().to_owned()); //FIXME: avoid copy
           log_lh += edge.dense_partitions[si].msg_to_parent.log_lh;
           msgs_to_combine.push(edge.dense_partitions[si].msg_to_child.dis.dot(&expQt));
@@ -277,11 +281,13 @@ mod tests {
 
   use super::*;
   use crate::alphabet::alphabet::AlphabetName;
-  use crate::graph::node;
+  use crate::graph::node::GraphNodeKey;
   use crate::gtr::get_gtr::{jc69, JC69Params};
+  use crate::gtr::gtr::{GTRParams, GTR};
   use crate::io::fasta::read_many_fasta_str;
   use crate::io::json::{json_write_str, JsonPretty};
   use crate::io::nwk::nwk_read_str;
+  use crate::pretty_assert_ulps_eq;
   use crate::utils::string::vec_to_string;
   use eyre::Report;
   use indoc::indoc;
@@ -352,6 +358,96 @@ mod tests {
       json_write_str(&expected, JsonPretty(false))?,
       json_write_str(&actual, JsonPretty(false))?
     );
+
+    Ok(())
+  }
+
+  #[test]
+  fn test_root_state() -> Result<(), Report> {
+    rayon::ThreadPoolBuilder::new().num_threads(1).build_global()?;
+
+    let aln = read_many_fasta_str(
+      indoc! {r#"
+      >root
+      ACAGCCATGTATTG--
+      >AB
+      ACATCCCTGTA-TG--
+      >A
+      ACATCGCCNNA--GAC
+      >B
+      GCATCCCTGTA-NG--
+      >CD
+      CCGGCCATGTATTG--
+      >C
+      CCGGCGATGTRTTG--
+      >D
+      TCGGCCGTGTRTTG--
+    "#},
+      &NUC_ALPHABET,
+    )?;
+    let graph: DenseGraph = nwk_read_str("((A:0.1,B:0.2)AB:0.1,(C:0.2,D:0.12)CD:0.05)root:0.01;")?;
+
+    let treat_gap_as_unknown = true;
+    let alphabet = Alphabet::new(AlphabetName::Nuc, treat_gap_as_unknown)?;
+
+    let pi = array![0.2, 0.3, 0.15, 0.35];
+
+    // symmetric rate matrix
+    let mu = 1.0;
+
+    let gtr = GTR::new(GTRParams {
+      alphabet: Alphabet::default(),
+      W: None,
+      pi: pi.clone(),
+      mu,
+    })
+    .unwrap();
+
+    let partitions = vec![PartitionLikelihoodWithAln::new(gtr, alphabet, aln)?];
+
+    let log_lh = run_marginal_dense(&graph, partitions)?;
+
+    // test variable position distribution at the root
+    let pos_zero_root = array![0.28212327, 0.21643546, 0.13800802, 0.36343326];
+    let root = &graph
+      .get_exactly_one_root()
+      .unwrap()
+      .read_arc()
+      .payload()
+      .read_arc()
+      .dense_partitions[0];
+    let pos: usize = 0;
+    dbg!(&root.profile.dis.slice(s![pos, 0..4]));
+
+    dbg!(log_lh);
+    // pretty_assert_ulps_eq!(-56.946298878390444, log_lh, epsilon = 1e-6);
+    pretty_assert_ulps_eq!(root.profile.dis.slice(s![pos, 0..4]), &pos_zero_root, epsilon = 1e-6);
+
+    // test variable position distribution at internal node
+    let pos_zero_ab = array![0.51275208, 0.09128506, 0.24647255, 0.14949031];
+    let node_ab = &graph
+      .get_node(GraphNodeKey(1))
+      .unwrap()
+      .read_arc()
+      .payload()
+      .read_arc()
+      .dense_partitions[0];
+    let pos: usize = 0;
+    pretty_assert_ulps_eq!(node_ab.profile.dis.slice(s![pos, 0..4]), &pos_zero_ab, epsilon = 1e-6);
+
+    // test variable position distribution at internal node
+    let dis_ab = array![
+      0.0013914677323952813,
+      0.002087201598592933,
+      0.042827146239885545,
+      0.9536941844291262
+    ];
+    let pos: usize = 3;
+    pretty_assert_ulps_eq!(node_ab.profile.dis.slice(s![pos, 0..4]), &dis_ab, epsilon = 1e-6);
+
+    //FIXME: this currently doesn't work
+    // let log_lh_ab = node_ab.profile.log_lh;
+    // pretty_assert_ulps_eq!(log_lh_ab, log_lh, epsilon=1e-8);
 
     Ok(())
   }
