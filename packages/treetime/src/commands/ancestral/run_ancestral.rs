390f93d5724ee0bc8289b3d1c6bdf5754d5d1d68
ivan-aksamentov <ivan.aksamentov@gmail.com>
2025-08-28 14:39:49 +0200

    wip

--- packages/treetime/src/commands/ancestral/run_ancestral.rs
+++ packages/treetime/src/commands/ancestral/run_ancestral.rs
@@ -2,17 +2,22 @@ use crate::alphabet::alphabet::Alphabet;
 use crate::commands::ancestral::anc_args::{MethodAncestral, TreetimeAncestralArgs};
 use crate::commands::ancestral::fitch::{ancestral_reconstruction_fitch, compress_sequences, get_common_length};
 use crate::commands::ancestral::marginal_dense::{ancestral_reconstruction_marginal_dense, run_marginal_dense};
+use crate::commands::ancestral::marginal_sparse::{
+  ancestral_reconstruction_marginal_sparse, compress_sequences_marginal_sparse, run_marginal_sparse,
+};
 use crate::graph::edge::GraphEdge;
 use crate::graph::graph::Graph;
 use crate::graph::node::GraphNode;
-use crate::gtr::get_gtr::get_gtr_dense;
+use crate::gtr::get_gtr::{JC69Params, get_gtr_dense, jc69};
 use crate::io::fasta::{FastaReader, FastaRecord, FastaWriter, read_many_fasta};
 use crate::io::file::{create_file_or_stdout, open_stdin};
 use crate::io::nex::{NexWriteOptions, nex_write_file};
 use crate::io::nwk::{EdgeToNwk, NodeToNwk, NwkWriteOptions, nwk_read_file, nwk_write_file};
 use crate::representation::graph_ancestral::GraphAncestral;
 use crate::representation::graph_dense::DenseGraph;
+
 use crate::representation::infer_dense::infer_dense;
+use crate::representation::partition_marginal_sparse::PartitionMarginalSparse;
 use crate::representation::partition_parsimony::PartitionParsimonyNew;
 use crate::representation::partitions_likelihood::PartitionLikelihoodWithAln;
 use eyre::Report;
@@ -64,6 +69,7 @@ pub fn run_ancestral_reconstruction(ancestral_args: &TreetimeAncestralArgs) -> R
 
   let graph: GraphAncestral = nwk_read_file(tree)?;
 
+  #[allow(clippy::iter_on_single_items)]
   let partitions_parsimony = [PartitionParsimonyNew {
     index: 0,
     alphabet: alphabet.clone(),
@@ -85,13 +91,37 @@ pub fn run_ancestral_reconstruction(ancestral_args: &TreetimeAncestralArgs) -> R
     })?;
   }
 
-  // let partitions_marginal_sparse = [PartitionMarginalSparse {
-  //   index: 2,
-  //   gtr: jc69(JC69Params::default())?,
-  //   alphabet: alphabet.clone(),
-  //   length: get_common_length(&aln)?,
-  // }];
-  //
+  #[allow(clippy::iter_on_single_items)]
+  let partitions_marginal_sparse = [PartitionMarginalSparse {
+    index: 1,
+    gtr: jc69(JC69Params::default())?,
+    alphabet: alphabet.clone(),
+    length: get_common_length(&aln)?,
+    nodes: btreemap! {},
+    edges: btreemap! {},
+  }]
+  .into_iter()
+  .map(|p| Arc::new(RwLock::new(p)))
+  .collect_vec();
+
+  if !partitions_marginal_sparse.is_empty() {
+    compress_sequences_marginal_sparse(&graph, &partitions_marginal_sparse, &aln)?;
+
+    run_marginal_sparse(&graph, &partitions_marginal_sparse)?;
+
+    ancestral_reconstruction_marginal_sparse(
+      &graph,
+      *reconstruct_tip_states,
+      &partitions_marginal_sparse,
+      |node, seq| {
+        let name = node.name.as_deref().unwrap_or("");
+        let desc = &node.desc;
+        output_fasta.write(name, desc, seq).unwrap();
+      },
+    )?;
+  }
+
+  // #[allow(clippy::iter_on_single_items)]
   // let partitions_marginal_dense = [PartitionMarginalDense {
   //   index: 2,
   //   gtr: jc69(JC69Params::default())?,
--- packages/treetime/src/representation/partition_marginal_sparse.rs
+++ packages/treetime/src/representation/partition_marginal_sparse.rs
@@ -1,5 +1,9 @@
 use crate::alphabet::alphabet::Alphabet;
+use crate::graph::edge::GraphEdgeKey;
+use crate::graph::node::GraphNodeKey;
 use crate::gtr::gtr::GTR;
+use crate::representation::graph_sparse::{SparseSeqEdge, SparseSeqNode};
+use std::collections::BTreeMap;
 
 #[derive(Clone, Debug)]
 pub struct PartitionMarginalSparse {
@@ -7,4 +11,6 @@ pub struct PartitionMarginalSparse {
   pub gtr: GTR,
   pub alphabet: Alphabet,
   pub length: usize,
+  pub nodes: BTreeMap<GraphNodeKey, SparseSeqNode>,
+  pub edges: BTreeMap<GraphEdgeKey, SparseSeqEdge>,
 }
