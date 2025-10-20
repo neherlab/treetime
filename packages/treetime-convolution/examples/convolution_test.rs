859d386cb32100cdf09aecebeb1e06f64f44702d
ivan-aksamentov <ivan.aksamentov@gmail.com>
2025-10-20 08:24:31 +0200

    feat: setup pretty logging

--- packages/treetime-convolution/examples/convolution_test.rs
+++ packages/treetime-convolution/examples/convolution_test.rs
@@ -1,5 +1,12 @@
+use ctor::ctor;
 use eyre::Report;
 use treetime_convolution::testing::run::run_convolution_tests;
+use treetime_utils::global_init::global_init;
+
+#[ctor]
+fn init() {
+  global_init();
+}
 
 fn main() -> Result<(), Report> {
   run_convolution_tests()
