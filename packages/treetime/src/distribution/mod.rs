746141aa8b382f8670c64ea4a1f62a35bfcf421f
ivan-aksamentov <ivan.aksamentov@gmail.com>
2025-12-10 08:44:12 +0100

    refactor: remove unused code

--- packages/treetime/src/distribution/mod.rs
+++ packages/treetime/src/distribution/mod.rs
@@ -8,7 +8,6 @@ pub mod distribution_multiplication;
 pub mod distribution_negation;
 pub mod distribution_point;
 pub mod distribution_range;
-pub mod distribution_resample;
 pub mod distribution_scalar_multiplication;
 pub mod distribution_subtraction;
 pub mod distribution_time_bounds;
