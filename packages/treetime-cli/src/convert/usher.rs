e5c1c47fc02564829ff7f06b41de0d64e8714189
ivan-aksamentov <ivan.aksamentov@gmail.com>
2026-01-28 10:41:03 +0100

    feat(converter): add mutation support for UShER format
    Parse and serialize nucleotide mutations when reading/writing UShER MAT
    protobuf files. Uses integer encoding (0:A, 1:C, 2:G, 3:T) per UShER
    convention.

--- packages/treetime-cli/src/convert/usher.rs
+++ packages/treetime-cli/src/convert/usher.rs
@@ -1,16 +1,75 @@
 use crate::convert::convert::{ConverterData, ConverterEdge, ConverterNode};
+use crate::convert::mutation::{Mutation, MutationList, PartitionedMutations};
 use eyre::Report;
+use itertools::Itertools;
+use maplit::btreemap;
 use treetime::graph::graph::Graph;
 use treetime::io::usher_mat::{
-  UsherGraphContext, UsherMetadata, UsherMutationList, UsherRead, UsherTree, UsherTreeContext, UsherTreeNode,
-  UsherWrite,
+  UsherGraphContext, UsherMetadata, UsherMutation, UsherMutationList, UsherRead, UsherTree, UsherTreeContext,
+  UsherTreeNode, UsherWrite,
 };
 use treetime::make_internal_report;
+
+const NUC_PARTITION: &str = "nuc";
+const NUC_CHARS: [u8; 4] = [b'A', b'C', b'G', b'T'];
+
+fn nuc_int_to_char(nuc: i32) -> Option<u8> {
+  NUC_CHARS.get(nuc as usize).copied()
+}
+
+fn nuc_char_to_int(nuc: u8) -> Option<i32> {
+  NUC_CHARS.iter().position(|&c| c == nuc).map(|i| i as i32)
+}
+
+fn parse_usher_mutations(usher_mutations: &[UsherMutation]) -> Result<PartitionedMutations, Report> {
+  if usher_mutations.is_empty() {
+    return Ok(btreemap! {});
+  }
+
+  let mutations: MutationList = usher_mutations
+    .iter()
+    .filter_map(|m| {
+      let reference = nuc_int_to_char(m.par_nuc)?;
+      let alternative = m.mut_nuc.first().and_then(|&n| nuc_int_to_char(n))?;
+      let position = m.position as usize;
+      Some(Mutation::new(reference, position, alternative))
+    })
+    .collect_vec();
+
+  if mutations.is_empty() {
+    Ok(btreemap! {})
+  } else {
+    Ok(btreemap! { NUC_PARTITION.to_owned() => mutations })
+  }
+}
+
+fn format_usher_mutations(mutations: &PartitionedMutations) -> UsherMutationList {
+  let nuc_mutations = mutations.get(NUC_PARTITION);
+
+  let mutation = nuc_mutations.map_or(vec![], |muts| {
+    muts
+      .iter()
+      .filter_map(|m| {
+        let par_nuc = nuc_char_to_int(m.reference)?;
+        let mut_nuc = nuc_char_to_int(m.alternative)?;
+        Some(UsherMutation {
+          position: m.position as i32,
+          ref_nuc: par_nuc,
+          par_nuc,
+          mut_nuc: vec![mut_nuc],
+          chromosome: String::new(),
+        })
+      })
+      .collect_vec()
+  });
+
+  UsherMutationList { mutation }
+}
 
 pub struct UsherWriter;
 
 impl UsherWrite<ConverterNode, ConverterEdge, ConverterData> for UsherWriter {
-  fn new(graph: &Graph<ConverterNode, ConverterEdge, ConverterData>) -> Result<Self, Report> {
+  fn new(_graph: &Graph<ConverterNode, ConverterEdge, ConverterData>) -> Result<Self, Report> {
     Ok(Self {})
   }
 
@@ -18,7 +77,7 @@ impl UsherWrite<ConverterNode, ConverterEdge, ConverterData> for UsherWriter {
     &mut self,
     context: &UsherGraphContext<ConverterNode, ConverterEdge, ConverterData>,
   ) -> Result<(UsherTreeNode, UsherMutationList, UsherMetadata), Report> {
-    let &UsherGraphContext { node, .. } = context;
+    let &UsherGraphContext { node, edge, .. } = context;
 
     let node_name = node
       .name
@@ -31,7 +90,9 @@ impl UsherWrite<ConverterNode, ConverterEdge, ConverterData> for UsherWriter {
       condensed_leaves: vec![],
     };
 
-    let usher_mutations = UsherMutationList { mutation: vec![] };
+    let usher_mutations = edge.map_or(UsherMutationList { mutation: vec![] }, |e| {
+      format_usher_mutations(&e.mutations)
+    });
 
     let usher_meta = UsherMetadata {
       clade_annotations: vec![],
@@ -41,15 +102,21 @@ impl UsherWrite<ConverterNode, ConverterEdge, ConverterData> for UsherWriter {
   }
 }
 
-pub struct UsherReader;
+pub struct UsherReader {
+  has_mutations: bool,
+}
 
 impl UsherRead<ConverterNode, ConverterEdge, ConverterData> for UsherReader {
   fn new(tree: &UsherTree) -> Result<Self, Report> {
-    Ok(Self {})
+    let has_mutations = tree.node_mutations.iter().any(|ml| !ml.mutation.is_empty());
+    Ok(Self { has_mutations })
   }
 
-  fn usher_data_to_graph_data(&mut self, tree: &UsherTree) -> Result<ConverterData, Report> {
-    Ok(ConverterData::default())
+  fn usher_data_to_graph_data(&mut self, _tree: &UsherTree) -> Result<ConverterData, Report> {
+    Ok(ConverterData {
+      has_mutations: self.has_mutations,
+      ..Default::default()
+    })
   }
 
   fn usher_node_to_graph_components(
@@ -58,13 +125,15 @@ impl UsherRead<ConverterNode, ConverterEdge, ConverterData> for UsherReader {
   ) -> Result<(ConverterNode, ConverterEdge), Report> {
     let UsherTreeContext { node, .. } = context;
 
+    let mutations = parse_usher_mutations(&node.mutations)?;
+
     let node = ConverterNode {
       name: node.name.as_ref().map(ToOwned::to_owned),
     };
 
     let edge = ConverterEdge {
       weight: None,
-      ..Default::default()
+      mutations,
     };
 
     Ok((node, edge))
