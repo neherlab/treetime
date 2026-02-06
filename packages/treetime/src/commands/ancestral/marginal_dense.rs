39dc523ac49da251ac0758dc49db091ead602c92
ivan-aksamentov <ivan.aksamentov@gmail.com>
2026-02-06 13:18:37 +0100

    test(ancestral): add dense marginal invariant tests
    - Add test helpers for normalization checks and non-uniform GTR creation
    - Add probability normalization test for node profiles and msg_to_child
    - Add update idempotence test verifying repeated marginal passes give same likelihood
    - Add root invariance test for reversible GTR models with different rootings

--- packages/treetime/src/commands/ancestral/marginal_dense.rs
+++ packages/treetime/src/commands/ancestral/marginal_dense.rs
@@ -3,21 +3,85 @@ mod tests {
   use crate::alphabet::alphabet::Alphabet;
   use crate::alphabet::alphabet::AlphabetName;
   use crate::commands::ancestral::fitch::get_common_length;
-  use crate::commands::ancestral::marginal_unified::{ancestral_reconstruction_marginal, initialize_marginal};
+  use crate::commands::ancestral::marginal_unified::{ancestral_reconstruction_marginal, initialize_marginal, update_marginal};
   use crate::gtr::get_gtr::{JC69Params, jc69};
-  use crate::io::fasta::read_many_fasta_str;
+  use crate::gtr::gtr::{GTRParams, GTR};
+  use crate::io::fasta::{read_many_fasta_str, FastaRecord};
   use crate::io::nwk::nwk_read_str;
   use crate::representation::graph_ancestral::GraphAncestral;
   use crate::representation::partition_marginal_dense::PartitionMarginalDense;
+  use approx::assert_ulps_eq;
   use eyre::Report;
   use indoc::indoc;
   use lazy_static::lazy_static;
   use maplit::btreemap;
+  use ndarray::prelude::*;
   use parking_lot::RwLock;
   use pretty_assertions::assert_eq;
   use std::collections::BTreeMap;
   use std::sync::Arc;
   use treetime_io::json::{JsonPretty, json_write_str};
+
+  fn assert_dense_rows_normalized(dis: &Array2<f64>, epsilon: f64) {
+    for (row_idx, row) in dis.rows().into_iter().enumerate() {
+      let sum: f64 = row.sum();
+      assert!(
+        (sum - 1.0).abs() < epsilon,
+        "Row {row_idx} sum={sum} is not normalized to 1.0 within epsilon={epsilon}"
+      );
+      for (col_idx, &val) in row.iter().enumerate() {
+        assert!(
+          val.is_finite(),
+          "Row {row_idx}, col {col_idx} has non-finite value: {val}"
+        );
+        assert!(
+          val >= -1e-15,
+          "Row {row_idx}, col {col_idx} has negative value: {val}"
+        );
+      }
+    }
+  }
+
+  fn make_nonuniform_gtr(treat_gap_as_unknown: bool) -> Result<GTR, Report> {
+    let alphabet = Alphabet::new(AlphabetName::Nuc, treat_gap_as_unknown)?;
+    GTR::new(GTRParams {
+      alphabet,
+      mu: 1.0,
+      W: None,
+      pi: array![0.2, 0.3, 0.15, 0.35],
+    })
+  }
+
+  fn run_dense_marginal(
+    graph: &GraphAncestral,
+    aln: &[FastaRecord],
+    gtr: GTR,
+    treat_gap_as_unknown: bool,
+  ) -> Result<(f64, [Arc<RwLock<PartitionMarginalDense>>; 1]), Report> {
+    let alphabet = Alphabet::new(AlphabetName::Nuc, treat_gap_as_unknown)?;
+    let partitions = [Arc::new(RwLock::new(PartitionMarginalDense {
+      index: 0,
+      gtr,
+      alphabet,
+      length: get_common_length(aln)?,
+      nodes: btreemap! {},
+      edges: btreemap! {},
+    }))];
+
+    let log_lh = initialize_marginal(graph, &partitions, aln)?;
+    Ok((log_lh, partitions))
+  }
+
+  fn run_dense_lh_for_newick(
+    newick: &str,
+    aln: &[FastaRecord],
+    gtr: GTR,
+    treat_gap_as_unknown: bool,
+  ) -> Result<f64, Report> {
+    let graph: GraphAncestral = nwk_read_str(newick)?;
+    let (log_lh, _) = run_dense_marginal(&graph, aln, gtr, treat_gap_as_unknown)?;
+    Ok(log_lh)
+  }
 
   lazy_static! {
     static ref NUC_ALPHABET: Alphabet = Alphabet::default();
@@ -92,6 +156,130 @@ mod tests {
       json_write_str(&expected, JsonPretty(false))?,
       json_write_str(&actual, JsonPretty(false))?
     );
+
+    Ok(())
+  }
+
+  #[test]
+  fn test_marginal_dense_probability_normalization() -> Result<(), Report> {
+    let aln = read_many_fasta_str(
+      indoc! {r#"
+      >root
+      TCAGCCATGTATTG--
+      >AB
+      ACATCCCTGTA-TG--
+      >A
+      ACATCGCCNNA--GAC
+      >B
+      GCATCCCTGTA-NG--
+      >CD
+      CCGGCCATGTATTG--
+      >C
+      CCGGCGATGTRTTG--
+      >D
+      TCGGCCGTGTRTTG--
+    "#},
+      &NUC_ALPHABET,
+    )?;
+
+    let graph: GraphAncestral = nwk_read_str("((A:0.1,B:0.2)AB:0.1,(C:0.2,D:0.12)CD:0.05)root:0.01;")?;
+    let gtr = jc69(JC69Params {
+      alphabet: AlphabetName::Nuc,
+      treat_gap_as_unknown: true,
+      ..JC69Params::default()
+    })?;
+
+    let (_, partitions) = run_dense_marginal(&graph, &aln, gtr, true)?;
+    let partition = partitions[0].read_arc();
+    let epsilon = 1e-10;
+
+    for (_node_key, node_data) in &partition.nodes {
+      if !node_data.profile.dis.is_empty() {
+        assert_dense_rows_normalized(&node_data.profile.dis, epsilon);
+      }
+    }
+
+    for (_edge_key, edge_data) in &partition.edges {
+      if !edge_data.msg_to_child.dis.is_empty() {
+        assert_dense_rows_normalized(&edge_data.msg_to_child.dis, epsilon);
+      }
+    }
+
+    Ok(())
+  }
+
+  #[test]
+  fn test_marginal_dense_update_is_idempotent() -> Result<(), Report> {
+    let aln = read_many_fasta_str(
+      indoc! {r#"
+      >root
+      TCAGCCATGTATTG--
+      >AB
+      ACATCCCTGTA-TG--
+      >A
+      ACATCGCCNNA--GAC
+      >B
+      GCATCCCTGTA-NG--
+      >CD
+      CCGGCCATGTATTG--
+      >C
+      CCGGCGATGTRTTG--
+      >D
+      TCGGCCGTGTRTTG--
+    "#},
+      &NUC_ALPHABET,
+    )?;
+
+    let graph: GraphAncestral = nwk_read_str("((A:0.1,B:0.2)AB:0.1,(C:0.2,D:0.12)CD:0.05)root:0.01;")?;
+    let gtr = jc69(JC69Params {
+      alphabet: AlphabetName::Nuc,
+      treat_gap_as_unknown: true,
+      ..JC69Params::default()
+    })?;
+
+    let (_, partitions) = run_dense_marginal(&graph, &aln, gtr, true)?;
+
+    let log_lh_first = update_marginal(&graph, &partitions)?;
+    let log_lh_second = update_marginal(&graph, &partitions)?;
+
+    assert_ulps_eq!(log_lh_first, log_lh_second, epsilon = 1e-10);
+
+    Ok(())
+  }
+
+  #[test]
+  fn test_marginal_dense_log_lh_root_invariance_reversible_model() -> Result<(), Report> {
+    let aln = read_many_fasta_str(
+      indoc! {r#"
+      >A
+      ACGTACGTACGTACGT
+      >B
+      ACGTACGTACGTACGA
+      >C
+      ACGTACGTACGTACGG
+      >D
+      ACGTACGTACGTACGC
+    "#},
+      &NUC_ALPHABET,
+    )?;
+
+    let treat_gap_as_unknown = true;
+    let gtr1 = make_nonuniform_gtr(treat_gap_as_unknown)?;
+    let gtr2 = make_nonuniform_gtr(treat_gap_as_unknown)?;
+    let gtr3 = make_nonuniform_gtr(treat_gap_as_unknown)?;
+
+    let tree1 = "((A:0.1,B:0.2)AB:0.1,(C:0.2,D:0.12)CD:0.05)root:0.01;";
+    let tree2 = "(A:0.1,B:0.2,(C:0.2,D:0.12)CD:0.15)AB:0.01;";
+    let tree3 = "((A:0.1,B:0.2)AB:0.15,C:0.2,D:0.12)CD:0.01;";
+
+    let log_lh1 = run_dense_lh_for_newick(tree1, &aln, gtr1, treat_gap_as_unknown)?;
+    let log_lh2 = run_dense_lh_for_newick(tree2, &aln, gtr2, treat_gap_as_unknown)?;
+    let log_lh3 = run_dense_lh_for_newick(tree3, &aln, gtr3, treat_gap_as_unknown)?;
+
+    let epsilon = 1e-6;
+    assert_ulps_eq!(log_lh1, log_lh2, epsilon = epsilon);
+    assert_ulps_eq!(log_lh1, log_lh3, epsilon = epsilon);
+    assert_ulps_eq!(log_lh2, log_lh3, epsilon = epsilon);
 
     Ok(())
   }
