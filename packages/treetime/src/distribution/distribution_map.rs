8b1580ecb9ad86307d34482249d087dfacd5ee70
ivan-aksamentov <ivan.aksamentov@gmail.com>
2026-02-13 15:12:32 +0100

    chore(distribution): remove unused distribution_from_fn function

--- packages/treetime/src/distribution/distribution_map.rs
+++ packages/treetime/src/distribution/distribution_map.rs
@@ -1,7 +1,6 @@
 use crate::distribution::distribution::Distribution;
 use crate::distribution::y_axis_policy::YAxisPolicy;
 use eyre::Report;
-use ndarray::Array1;
 
 pub fn distribution_map<Y: YAxisPolicy, F>(dist: &Distribution<Y>, f: F) -> Result<Distribution<Y>, Report>
 where
@@ -14,12 +13,4 @@ where
     Distribution::Empty => Ok(Distribution::empty()),
     Distribution::Formula(_) => panic!("Map not implemented for Formula distributions"),
   }
-}
-
-pub fn distribution_from_fn<Y: YAxisPolicy, F>(time_points: Array1<f64>, f: F) -> Result<Distribution<Y>, Report>
-where
-  F: Fn(f64) -> f64,
-{
-  let y_values = time_points.mapv(&f);
-  Distribution::function(time_points, y_values)
 }
