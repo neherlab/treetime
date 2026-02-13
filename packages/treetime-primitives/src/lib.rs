ae39211106849f18c5bfdb52a10d487c404aa8d6
ivan-aksamentov <ivan.aksamentov@gmail.com>
2026-02-13 13:35:18 +0100

    feat(primitives): add StateSet type aliases and stateset! macro
    - Add StateSet and StateSetStatus type aliases for BitSet128
    - Add stateset! macro that delegates to bitset128!

--- packages/treetime-primitives/src/lib.rs
+++ packages/treetime-primitives/src/lib.rs
@@ -5,3 +5,13 @@ mod seq_char;
 pub use bitset128::{BitSet128, Bitset128Status};
 pub use seq::Seq;
 pub use seq_char::AsciiChar;
+
+pub type StateSet = BitSet128;
+pub type StateSetStatus = Bitset128Status;
+
+#[macro_export]
+macro_rules! stateset {
+  ($($args:tt)*) => {
+    $crate::bitset128!($($args)*)
+  };
+}
