a330701722ad9defa2bca6b0def9cf197f53d60f
ivan-aksamentov <ivan.aksamentov@gmail.com>
2026-02-13 15:08:49 +0100

    refactor(io): move io modules from treetime to treetime-io
    Move 13 io module files:
    - auspice, auspice_types, concat, csv, dates_csv, discrete_states_csv
    - fasta, graphviz, nex, nwk, parse_delimited, phyloxml, usher_mat
    
    Tests remain in treetime/src/io/__tests__/.
    Dependencies and imports to be fixed in subsequent commits.

--- packages/treetime/src/io/mod.rs
+++ packages/treetime/src/io/mod.rs
@@ -1,15 +1,1 @@
-pub mod auspice;
-pub mod auspice_types;
-pub mod concat;
-pub mod csv;
-pub mod dates_csv;
-pub mod discrete_states_csv;
-pub mod fasta;
-pub mod graphviz;
-pub mod nex;
-pub mod nwk;
-pub mod parse_delimited;
-pub mod phyloxml;
-pub mod usher_mat;
-
 mod __tests__;
