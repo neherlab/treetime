7c4bbab4bd87fa094a0e7e17dfa67f5446265b18
ivan-aksamentov <ivan.aksamentov@gmail.com>
2026-02-13 13:37:11 +0100

    feat(primitives): wire treetime-primitives dependency in treetime crate

--- packages/treetime/src/lib.rs
+++ packages/treetime/src/lib.rs
@@ -11,6 +11,7 @@ pub mod representation;
 pub mod seq;
 
 pub use treetime_graph;
+pub use treetime_primitives;
 pub use treetime_utils::{
   make_error, make_internal_error, make_internal_report, make_report, o, pretty_assert_abs_diff_eq,
   pretty_assert_ulps_eq, vec_of_owned, vec_u8,
