dbdb80695011381b7a80b042d668635b1dbaa92d
ivan-aksamentov <ivan.aksamentov@gmail.com>
2026-02-14 18:43:16 +0100

    chore(primitives): remove commented-out Seq::splice method

--- packages/treetime-primitives/src/seq.rs
+++ packages/treetime-primitives/src/seq.rs
@@ -147,17 +147,6 @@ impl Seq {
       data: self.data.split_off(at),
     }
   }
-
-  // pub fn splice<I>(
-  //   &mut self,
-  //   range: core::ops::Range<usize>,
-  //   replace_with: I,
-  // ) -> std::vec::Splice<'_, <I as IntoIterator>::IntoIter>
-  // where
-  //   I: IntoIterator<Item = u8>,
-  // {
-  //   self.data.splice(range, replace_with.into_iter().map(AsciiChar::from))
-  // }
 }
 
 impl PartialEq for Seq {
