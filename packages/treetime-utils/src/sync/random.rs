27a9ebe93cd1644c61f8f6252e44d00bd6f477f5
ivan-aksamentov <ivan.aksamentov@gmail.com>
2026-02-16 10:49:54 +0100

    chore(utils): remove unused random_pop and random_remove functions

--- packages/treetime-utils/src/sync/random.rs
+++ packages/treetime-utils/src/sync/random.rs
@@ -1,10 +1,8 @@
 use crate::make_internal_report;
 use eyre::Report;
-use itertools::Itertools;
 use ndarray_rand::rand::SeedableRng;
 use ndarray_rand::rand::{Rng, seq::IteratorRandom, seq::SliceRandom};
 use rand_isaac::Isaac64Rng;
-use std::collections::BTreeSet;
 
 pub fn get_random_number_generator(seed: Option<u64>) -> impl Rng + Send + Sync + Clone {
   match seed {
@@ -24,21 +22,6 @@ pub fn random_choice_maybe<T>(iter: impl IntoIterator<Item = T>, rng: &mut impl 
 pub fn random_choice<T>(iter: impl IntoIterator<Item = T>, rng: &mut impl Rng) -> Result<T, Report> {
   random_choice_maybe(iter, rng)
     .ok_or_else(|| make_internal_report!("random_choice: expected at least one item, but none found"))
-}
-
-pub fn random_remove<T>(v: &mut Vec<T>, rng: &mut impl Rng) -> T {
-  let index: usize = rng.gen_range(0..v.len());
-  v.remove(index)
-}
-
-pub fn random_pop<T: Clone + Ord>(s: &mut BTreeSet<T>, rng: &mut impl Rng) -> T {
-  // TODO: inefficient. Try to avoid copying. Sets have no indexed access, so removing random element is tricky.
-  // Might be possible with IndexSet?
-  let mut v = s.iter().cloned().collect_vec();
-  let item = random_remove(&mut v, rng);
-  *s = BTreeSet::new();
-  s.extend(v);
-  item
 }
 
 pub fn random_sequence(length: usize, rng: &mut impl Rng) -> Vec<char> {
