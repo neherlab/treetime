1d428d80099643184002b927e6cdb990fba4e394
ivan-aksamentov <ivan.aksamentov@gmail.com>
2026-04-08 18:15:49 +0200

    chore: drop Python 3.7 from metadata (unavailable on ubuntu-24.04)

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
