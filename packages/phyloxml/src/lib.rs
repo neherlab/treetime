69858216141a347ae51df6f38802bd06f9e6a823
ivan-aksamentov <ivan.aksamentov@gmail.com>
2026-04-22 13:58:42 +0200

    refactor: add std::io import in phyloxml lib.rs

--- packages/phyloxml/src/lib.rs
+++ packages/phyloxml/src/lib.rs
@@ -3,27 +3,29 @@ pub mod types;
 pub use crate::types::*;
 use quick_xml::DeError;
 use quick_xml::de::from_reader;
+use std::io;
 
-pub fn phyloxml_read(reader: impl std::io::Read) -> Result<Phyloxml, DeError> {
-  let reader = std::io::BufReader::new(reader);
+pub fn phyloxml_read(reader: impl io::Read) -> Result<Phyloxml, DeError> {
+  let reader = io::BufReader::new(reader);
   from_reader(reader)
 }
 
-pub fn phyloxml_write(writer: impl std::io::Write, phyloxml: &Phyloxml) -> std::io::Result<()> {
+pub fn phyloxml_write(writer: impl io::Write, phyloxml: &Phyloxml) -> io::Result<()> {
   details::to_writer_pretty(writer, "phyloxml", phyloxml)
 }
 
 mod details {
   use quick_xml::se::to_string_with_root;
   use serde::Serialize;
+  use std::io;
   use std::io::Cursor;
 
-  pub fn to_writer_pretty<W, T>(writer: W, root_tag: &str, data: &T) -> std::io::Result<()>
+  pub fn to_writer_pretty<W, T>(writer: W, root_tag: &str, data: &T) -> io::Result<()>
   where
-    W: std::io::Write,
+    W: io::Write,
     T: Serialize,
   {
-    let s = to_string_with_root(root_tag, data).map_err(std::io::Error::other)?;
+    let s = to_string_with_root(root_tag, data).map_err(io::Error::other)?;
 
     let reader = xml::ParserConfig::new()
       .trim_whitespace(true)
@@ -44,10 +46,10 @@ mod details {
     Ok(())
   }
 
-  fn to_io<E>(e: E) -> std::io::Error
+  fn to_io<E>(e: E) -> io::Error
   where
     E: Into<Box<dyn std::error::Error + Send + Sync>>,
   {
-    std::io::Error::other(e)
+    io::Error::other(e)
   }
 }
