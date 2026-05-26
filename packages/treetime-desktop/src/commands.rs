bec9810836ed8bc804a9602b4d5bad5f6c831d5a
ivan-aksamentov <ivan.aksamentov@gmail.com>
2026-05-26 11:51:15 +0200

    refactor(desktop): wire treetime-desktop through treetime-api
    - Desktop napi addon depends on treetime-api instead of treetime directly
    - Replace repetitive Task impls with define_task! macro
    - Add axum, tokio, tower-http, treetime-api to workspace dependencies
    - Remove old frontend/ directory (superseded by treetime-desktop-app)

--- packages/treetime-desktop/src/commands.rs
+++ packages/treetime-desktop/src/commands.rs
@@ -1,17 +1,10 @@
 use napi::Task;
 use napi_derive::napi;
-use treetime::commands::ancestral::args::TreetimeAncestralArgs;
-use treetime::commands::ancestral::run::run_ancestral_reconstruction;
-use treetime::commands::clock::args::TreetimeClockArgs;
-use treetime::commands::clock::run::run_clock;
-use treetime::commands::mugration::args::TreetimeMugrationArgs;
-use treetime::commands::mugration::run::run_mugration;
-use treetime::commands::optimize::args::TreetimeOptimizeArgs;
-use treetime::commands::optimize::run::run_optimize;
-use treetime::commands::prune::args::TreetimePruneArgs;
-use treetime::commands::prune::run::run_prune;
-use treetime::commands::timetree::args::TreetimeTimetreeArgs;
-use treetime::commands::timetree::run::run_timetree_estimation;
+use treetime_api::progress::NoopProgress;
+use treetime_api::{
+  TreetimeAncestralArgs, TreetimeClockArgs, TreetimeMugrationArgs, TreetimeOptimizeArgs, TreetimePruneArgs,
+  TreetimeTimetreeArgs,
+};
 
 fn eyre_to_napi(err: eyre::Report) -> napi::Error {
   napi::Error::new(napi::Status::GenericFailure, format!("{err:?}"))
@@ -21,141 +14,56 @@ fn json_to_napi(err: serde_json::Error) -> napi::Error {
   napi::Error::new(napi::Status::InvalidArg, format!("{err}"))
 }
 
-pub struct AncestralTask {
-  args: TreetimeAncestralArgs,
+macro_rules! define_task {
+  ($task_name:ident, $args_type:ty, $api_fn:path, $napi_fn:ident) => {
+    pub struct $task_name {
+      args: $args_type,
+    }
+
+    impl Task for $task_name {
+      type Output = ();
+      type JsValue = ();
+
+      fn compute(&mut self) -> napi::Result<Self::Output> {
+        $api_fn(&self.args, &NoopProgress).map_err(eyre_to_napi)
+      }
+
+      fn resolve(&mut self, _env: napi::Env, _output: ()) -> napi::Result<()> {
+        Ok(())
+      }
+    }
+
+    #[napi]
+    pub fn $napi_fn(args_json: String) -> napi::Result<napi::bindgen_prelude::AsyncTask<$task_name>> {
+      let args: $args_type = serde_json::from_str(&args_json).map_err(json_to_napi)?;
+      Ok(napi::bindgen_prelude::AsyncTask::new($task_name { args }))
+    }
+  };
 }
 
-impl Task for AncestralTask {
-  type Output = ();
-  type JsValue = ();
-
-  fn compute(&mut self) -> napi::Result<Self::Output> {
-    run_ancestral_reconstruction(&self.args).map_err(eyre_to_napi)
-  }
-
-  fn resolve(&mut self, _env: napi::Env, _output: ()) -> napi::Result<()> {
-    Ok(())
-  }
-}
-
-#[napi]
-pub fn ancestral(args_json: String) -> napi::Result<napi::bindgen_prelude::AsyncTask<AncestralTask>> {
-  let args: TreetimeAncestralArgs = serde_json::from_str(&args_json).map_err(json_to_napi)?;
-  Ok(napi::bindgen_prelude::AsyncTask::new(AncestralTask { args }))
-}
-
-pub struct ClockTask {
-  args: TreetimeClockArgs,
-}
-
-impl Task for ClockTask {
-  type Output = ();
-  type JsValue = ();
-
-  fn compute(&mut self) -> napi::Result<Self::Output> {
-    run_clock(&self.args).map_err(eyre_to_napi)?;
-    Ok(())
-  }
-
-  fn resolve(&mut self, _env: napi::Env, _output: ()) -> napi::Result<()> {
-    Ok(())
-  }
-}
-
-#[napi]
-pub fn clock(args_json: String) -> napi::Result<napi::bindgen_prelude::AsyncTask<ClockTask>> {
-  let args: TreetimeClockArgs = serde_json::from_str(&args_json).map_err(json_to_napi)?;
-  Ok(napi::bindgen_prelude::AsyncTask::new(ClockTask { args }))
-}
-
-pub struct TimetreeTask {
-  args: TreetimeTimetreeArgs,
-}
-
-impl Task for TimetreeTask {
-  type Output = ();
-  type JsValue = ();
-
-  fn compute(&mut self) -> napi::Result<Self::Output> {
-    run_timetree_estimation(&self.args).map_err(eyre_to_napi)
-  }
-
-  fn resolve(&mut self, _env: napi::Env, _output: ()) -> napi::Result<()> {
-    Ok(())
-  }
-}
-
-#[napi]
-pub fn timetree(args_json: String) -> napi::Result<napi::bindgen_prelude::AsyncTask<TimetreeTask>> {
-  let args: TreetimeTimetreeArgs = serde_json::from_str(&args_json).map_err(json_to_napi)?;
-  Ok(napi::bindgen_prelude::AsyncTask::new(TimetreeTask { args }))
-}
-
-pub struct MugrationTask {
-  args: TreetimeMugrationArgs,
-}
-
-impl Task for MugrationTask {
-  type Output = ();
-  type JsValue = ();
-
-  fn compute(&mut self) -> napi::Result<Self::Output> {
-    run_mugration(&self.args).map_err(eyre_to_napi)
-  }
-
-  fn resolve(&mut self, _env: napi::Env, _output: ()) -> napi::Result<()> {
-    Ok(())
-  }
-}
-
-#[napi]
-pub fn mugration(args_json: String) -> napi::Result<napi::bindgen_prelude::AsyncTask<MugrationTask>> {
-  let args: TreetimeMugrationArgs = serde_json::from_str(&args_json).map_err(json_to_napi)?;
-  Ok(napi::bindgen_prelude::AsyncTask::new(MugrationTask { args }))
-}
-
-pub struct OptimizeTask {
-  args: TreetimeOptimizeArgs,
-}
-
-impl Task for OptimizeTask {
-  type Output = ();
-  type JsValue = ();
-
-  fn compute(&mut self) -> napi::Result<Self::Output> {
-    run_optimize(&self.args).map_err(eyre_to_napi)
-  }
-
-  fn resolve(&mut self, _env: napi::Env, _output: ()) -> napi::Result<()> {
-    Ok(())
-  }
-}
-
-#[napi]
-pub fn optimize(args_json: String) -> napi::Result<napi::bindgen_prelude::AsyncTask<OptimizeTask>> {
-  let args: TreetimeOptimizeArgs = serde_json::from_str(&args_json).map_err(json_to_napi)?;
-  Ok(napi::bindgen_prelude::AsyncTask::new(OptimizeTask { args }))
-}
-
-pub struct PruneTask {
-  args: TreetimePruneArgs,
-}
-
-impl Task for PruneTask {
-  type Output = ();
-  type JsValue = ();
-
-  fn compute(&mut self) -> napi::Result<Self::Output> {
-    run_prune(&self.args).map_err(eyre_to_napi)
-  }
-
-  fn resolve(&mut self, _env: napi::Env, _output: ()) -> napi::Result<()> {
-    Ok(())
-  }
-}
-
-#[napi]
-pub fn prune(args_json: String) -> napi::Result<napi::bindgen_prelude::AsyncTask<PruneTask>> {
-  let args: TreetimePruneArgs = serde_json::from_str(&args_json).map_err(json_to_napi)?;
-  Ok(napi::bindgen_prelude::AsyncTask::new(PruneTask { args }))
-}
+define_task!(
+  AncestralTask,
+  TreetimeAncestralArgs,
+  treetime_api::commands::ancestral,
+  ancestral
+);
+define_task!(ClockTask, TreetimeClockArgs, treetime_api::commands::clock, clock);
+define_task!(
+  TimetreeTask,
+  TreetimeTimetreeArgs,
+  treetime_api::commands::timetree,
+  timetree
+);
+define_task!(
+  MugrationTask,
+  TreetimeMugrationArgs,
+  treetime_api::commands::mugration,
+  mugration
+);
+define_task!(
+  OptimizeTask,
+  TreetimeOptimizeArgs,
+  treetime_api::commands::optimize,
+  optimize
+);
+define_task!(PruneTask, TreetimePruneArgs, treetime_api::commands::prune, prune);
