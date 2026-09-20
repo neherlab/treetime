6b929daaf67584932442a871b3ac49d320aa7c07
ivan-aksamentov <ivan.aksamentov@gmail.com>
2026-09-21 01:50:43 +0200

    test(grid): compare accessor floats by ulps

--- packages/treetime-grid/src/__tests__/piecewise_fn.rs
+++ packages/treetime-grid/src/__tests__/piecewise_fn.rs
@@ -2,14 +2,15 @@
 mod tests {
   use crate::piecewise_fn::PiecewiseFnBase;
   use ndarray::array;
+  use treetime_utils::pretty_assert_ulps_eq;
 
   #[test]
   fn test_piecewise_fn_base_accessors() {
     let base = PiecewiseFnBase::new(array![1.0, 5.0, 10.0], array![0.0, 1.0, 2.0, 3.0]);
-    assert_eq!(base.breakpoints(), &array![1.0, 5.0, 10.0]);
-    assert_eq!(base.values(), &array![0.0, 1.0, 2.0, 3.0]);
-    assert_eq!(&[1.0, 5.0, 10.0], base.breakpoints_slice());
-    assert_eq!(&[0.0, 1.0, 2.0, 3.0], base.values_slice());
+    pretty_assert_ulps_eq!(&array![1.0, 5.0, 10.0], base.breakpoints(), max_ulps = 4);
+    pretty_assert_ulps_eq!(&array![0.0, 1.0, 2.0, 3.0], base.values(), max_ulps = 4);
+    pretty_assert_ulps_eq!(&[1.0, 5.0, 10.0][..], base.breakpoints_slice(), max_ulps = 4);
+    pretty_assert_ulps_eq!(&[0.0, 1.0, 2.0, 3.0][..], base.values_slice(), max_ulps = 4);
   }
 
   #[test]
