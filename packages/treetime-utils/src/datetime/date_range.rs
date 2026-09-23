605a3de6932124ea1722b25c24132e353df82f2d
ivan-aksamentov <ivan.aksamentov@gmail.com>
2026-09-23 03:26:28 +0200

    refactor(utils): build date ranges without the deprecated chrono ymd_opt

--- packages/treetime-utils/src/datetime/date_range.rs
+++ packages/treetime-utils/src/datetime/date_range.rs
@@ -1,5 +1,5 @@
 use crate::datetime::datetime::iso;
-use chrono::{DateTime, Duration, TimeZone, Utc};
+use chrono::{DateTime, Duration, NaiveDate, Utc};
 use getset::Getters;
 use serde::{Deserialize, Serialize};
 use smart_default::SmartDefault;
@@ -23,24 +23,23 @@ impl DateRange {
     Self::new(iso(begin.as_ref()), iso(end.as_ref()))
   }
 
-  #[allow(deprecated)]
   #[allow(
     clippy::unwrap_used,
     clippy::as_conversions,
     reason = "constructs a range from a caller-supplied valid (year, month, day); the year fits i32 and the components form a valid calendar instant"
   )]
   pub fn from_ymd(begin: (u32, u32, u32), end: (u32, u32, u32)) -> Self {
-    let begin = Utc
-      .ymd_opt(begin.0 as i32, begin.1, begin.2)
+    let begin = NaiveDate::from_ymd_opt(begin.0 as i32, begin.1, begin.2)
       .unwrap()
       .and_hms_opt(0, 0, 0)
-      .unwrap();
+      .unwrap()
+      .and_utc();
 
-    let end = Utc
-      .ymd_opt(end.0 as i32, end.1, end.2)
+    let end = NaiveDate::from_ymd_opt(end.0 as i32, end.1, end.2)
       .unwrap()
       .and_hms_nano_opt(23, 59, 59, 999_999_999)
-      .unwrap();
+      .unwrap()
+      .and_utc();
 
     Self::new(begin, end)
   }
