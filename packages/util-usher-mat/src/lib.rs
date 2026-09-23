f303487472708504519dfd79b7438b39e5da79a4
ivan-aksamentov <ivan.aksamentov@gmail.com>
2026-09-23 04:19:16 +0200

    refactor(util-usher-mat): order items callers before callees

--- packages/util-usher-mat/src/lib.rs
+++ packages/util-usher-mat/src/lib.rs
@@ -46,11 +46,33 @@ mod taxodium {
   include!(concat!(env!("OUT_DIR"), "/taxodium.rs"));
 }
 
-pub type UsherTree = parsimony::Data;
 pub type UsherTreeNode = parsimony::CondensedNode;
 pub type UsherMutation = parsimony::Mut;
 pub type UsherMutationList = parsimony::MutationList;
 pub type UsherMetadata = parsimony::NodeMetadata;
+pub fn usher_mat_pb_read(mut reader: impl Read) -> Result<UsherTree, Report> {
+  let mut buf = Vec::new();
+  reader
+    .read_to_end(&mut buf)
+    .wrap_err("When reading Usher MAT protobuf input")?;
+  usher_mat_pb_read_bytes(&buf[..])
+}
+
+pub fn usher_mat_pb_read_bytes(buf: impl Buf) -> Result<UsherTree, Report> {
+  UsherTree::decode(buf).wrap_err("When decoding Usher MAT protobuf message")
+}
+
+pub fn usher_mat_pb_write(writer: &mut impl Write, tree: &UsherTree) -> Result<(), Report> {
+  let mut buf = Vec::<u8>::with_capacity(tree.encoded_len());
+  usher_mat_pb_write_bytes(&mut buf, tree)?;
+  writer.write_all(&buf).wrap_err("When writing encoded protobuf message")
+}
+
+pub fn usher_mat_pb_write_bytes(buf: &mut impl BufMut, tree: &UsherTree) -> Result<(), Report> {
+  tree.encode(buf).wrap_err("When encoding Usher MAT protobuf message")
+}
+
+pub type UsherTree = parsimony::Data;
 
 impl UsherTree {
   pub fn get_all_positions(&self) -> BTreeSet<i32> {
@@ -60,26 +82,4 @@ impl UsherTree {
       .flat_map(|ml| ml.mutation.iter().map(|m| m.position))
       .collect()
   }
-}
-
-pub fn usher_mat_pb_read_bytes(buf: impl Buf) -> Result<UsherTree, Report> {
-  UsherTree::decode(buf).wrap_err("When decoding Usher MAT protobuf message")
-}
-
-pub fn usher_mat_pb_read(mut reader: impl Read) -> Result<UsherTree, Report> {
-  let mut buf = Vec::new();
-  reader
-    .read_to_end(&mut buf)
-    .wrap_err("When reading Usher MAT protobuf input")?;
-  usher_mat_pb_read_bytes(&buf[..])
-}
-
-pub fn usher_mat_pb_write_bytes(buf: &mut impl BufMut, tree: &UsherTree) -> Result<(), Report> {
-  tree.encode(buf).wrap_err("When encoding Usher MAT protobuf message")
-}
-
-pub fn usher_mat_pb_write(writer: &mut impl Write, tree: &UsherTree) -> Result<(), Report> {
-  let mut buf = Vec::<u8>::with_capacity(tree.encoded_len());
-  usher_mat_pb_write_bytes(&mut buf, tree)?;
-  writer.write_all(&buf).wrap_err("When writing encoded protobuf message")
 }
